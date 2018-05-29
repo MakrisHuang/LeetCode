8d2ef78b2390c53e4a5a835bb045dedd40e7e128
Makris <vallwesture@gmail.com>
2018-05-29 11:04:00 +0800

    add sol for prob 98 validate binary search tree

---
+++ 98_validate_binary_search_tree.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return valid(root, null, null);
+    }
+
+    private boolean valid(TreeNode curr, Integer low, Integer high){
+        if (curr == null) return true;
+        return (low == null || curr.val > low) &&
+               (high == null || curr.val < high) &&
+               valid(curr.left, low, curr.val) &&
+               valid(curr.right, curr.val, high);
+    }
+}
