81136f72272220cf4764c475c2df51dcf0c9a79b
Makris <vallwesture@gmail.com>
2018-06-11 23:38:57 +0800

    add sol for prob 151 reverse words in a string

---
+++ 151_reverse_words_in_a_string.java
@@ -0,0 +1,47 @@
+public class Solution {
+    public String reverseWords(String s) {
+        if (s == null) return null;
+        if (s.length() == 0) return s;
+
+        // reverse the entire sentence
+        s = reverse(s);
+
+        List<String> list = new ArrayList<>();
+        for (int begin = 0, end = 0; begin < s.length(); ) {
+            if (s.charAt(begin) == ' ') {
+                begin++; end++;
+            } else if (end == s.length() || s.charAt(end) == ' ') {
+                String str = s.substring(begin, end);
+                list.add(reverse(str));
+                begin = ++end;
+            } else {
+                end++;
+            }
+        }
+
+        // concat reversed words in the list
+        StringBuilder sb = new StringBuilder();
+        int len = list.size();
+        for(int i = 0; i < len; i++) {
+            if (i != len - 1) sb.append(list.get(i) + " ");
+            else sb.append(list.get(i));
+        }
+
+        return sb.toString();
+    }
+
+    private String reverse(String word) {
+        if (word == null) return "";
+        StringBuilder sb = new StringBuilder(word);
+        int begin = 0;
+        int end = word.length() - 1;
+        while (begin < end) {
+            Character temp = word.charAt(begin);
+            sb.setCharAt(begin, word.charAt(end));
+            sb.setCharAt(end, temp);
+            begin++;
+            end--;
+        }
+        return sb.toString();
+    }
+}
