e0c049405e73449ca5115fc87df55ca296d68452
Makris <vallwesture@gmail.com>
2018-06-17 11:05:08 +0800

    add sol for prob 154 find minimum in rotated sorted array II

---
+++ 154_find_minimum_in_rotated_sorted_array_II.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMin(int[] A) {
+        int L = 0, R = A.length - 1;
+        while (L < R && A[L] >= A[R]) {
+            int M = (L + R) / 2;
+            if (A[M] > A[R]) {
+                L = M + 1;
+            } else if (A[M] < A[L]) {
+                R = M;
+            } else {        // A[L] == A[M] == A[R]
+                            // move step by step, which may cause O(n) completity
+                L = L + 1;
+            }
+        }
+        return A[L];
+    }
+}
