452f2f84e0839e0f108c0b578f8b42ce4ce0d2e0
Makris <vallwesture@gmail.com>
2018-07-10 15:04:56 +0800

    add bfs sol for prob 513 find bottom left tree value

--- 513_find_bottom_left_tree_value.java
+++ 513_find_bottom_left_tree_value.java
@@ -9,25 +9,47 @@
  */
 class Solution {
     public int findBottomLeftValue(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        visitRow(root, 0, result);
-
-        List<Integer> lastLayer = result.get(result.size() - 1);
-        return lastLayer.get(0);
+        dfs(root, 1);
+//         List<List<Integer>> result = new ArrayList<>();
+//         visitRow(root, 0, result);
+    
+//         List<Integer> lastLayer = result.get(result.size() - 1);
+//         return lastLayer.get(0);
+        return value;
     }
-
-    private void visitRow(TreeNode node, int level, List<List<Integer>> result) {
-        if (node == null) return;
-        else {
-            if (level >= result.size()) {
-                result.add(new ArrayList<>());
-            }
-
-            List<Integer> layer = result.get(level);
-            layer.add(node.val);
-            level++;
-            visitRow(node.left, level, result);
-            visitRow(node.right, level, result);
+    
+    int value = 0;
+    int maxLevel = 0;
+    
+    private void dfs(TreeNode node, int level) {
+        if (level > maxLevel) {
+            maxLevel = level;
+            value = node.val;
+        }
+        
+        level++;
+        
+        if (node.left != null) {
+            dfs(node.left, level);    
+        }
+        
+        if (node.right != null) {
+            dfs(node.right, level);
         }
     }
+    
+//     private void visitRow(TreeNode node, int level, List<List<Integer>> result) {
+//         if (node == null) return;
+//         else {
+//             if (level >= result.size()) {
+//                 result.add(new ArrayList<>());
+//             }
+            
+//             List<Integer> layer = result.get(level);
+//             layer.add(node.val);
+//             level++;
+//             visitRow(node.left, level, result);
+//             visitRow(node.right, level, result);
+//         }
+//     }
 }
