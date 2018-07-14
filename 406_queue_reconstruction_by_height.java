0c5efe2941b30769738025dd3d41b90ceb53091f
Makris <vallwesture@gmail.com>
2018-07-14 11:14:35 +0800

    add concept as comment

--- 406_queue_reconstruction_by_height.java
+++ 406_queue_reconstruction_by_height.java
@@ -1,8 +1,12 @@
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
+        // sort the array, the tallest must come first
+        // and for k, higher k will stand behind
+        // see discussion for more details
         Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
         List<int[]> list = new LinkedList<>();
         for (int[] p : people) {
+            // insert by k -> means there will be k people stand in front of that person
             list.add(p[1], p);
         }
         return list.toArray(new int[list.size()][]);
