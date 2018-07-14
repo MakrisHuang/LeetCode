dd95c804dcee3934f915f7d9d28849ba04efce86
Makris <vallwesture@gmail.com>
2018-07-14 09:35:25 +0800

    add sol for prob 485 max consecutive ones

---
+++ 485_max_consecutive_ones.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        int count = 0, max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                if (count > max) {
+                    max = count;
+                }
+                count = 0;
+            }
+        }
+
+        return Math.max(max, count);
+    }
+}
