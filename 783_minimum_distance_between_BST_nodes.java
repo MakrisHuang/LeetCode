2935169c259f01641b770c16fd94744f4eaf54c0
Makris <vallwesture@gmail.com>
2018-07-18 11:18:07 +0800

    add sol for prob 783 minimum distance between BST nodes

---
+++ 783_minimum_distance_between_BST_nodes.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    Integer res = Integer.MAX_VALUE, pre = null;
+
+    public int minDiffInBST(TreeNode root) {
+        if (root.left != null) minDiffInBST(root.left);
+        if (pre != null) res = Math.min(res, root.val - pre);
+        pre = root.val;
+        if (root.right != null) minDiffInBST(root.right);
+        return res;
+    }
+}
