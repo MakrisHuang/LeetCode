f7ea1bbf0f0fd7bdc1ef1e8be3ad236b2781f993
Makris <vallwesture@gmail.com>
2018-07-22 19:06:27 +0800

    add sol for prob 139 word break

---
+++ 139_word_break.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[s.length()];
+        queue.offer(0);
+
+        while (! queue.isEmpty()) {
+            int start = queue.poll();
+            if (!visited[start]) {
+                for (int end = start + 1; end <= s.length(); end++) {
+                    if (wordSet.contains(s.substring(start, end))) {
+                        queue.offer(end);
+                        if (end == s.length()) return true;
+                    }
+                }
+                visited[start] = true;
+            }
+        }
+        return false;
+    }
+}
