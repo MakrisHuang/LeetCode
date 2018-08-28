05fd884178f380e1768c7dc03b4037971f750999
Makris <vallwesture@gmail.com>
2018-08-27 18:54:10 -0700

    add sol for prob 303 sum query

---
+++ 303_range_sum_query.java
@@ -0,0 +1,20 @@
+class NumArray {
+    int[] prefixSum;
+    public NumArray(int[] nums) {
+        prefixSum = nums;
+        for (int i = 1; i < prefixSum.length; i++) {
+            prefixSum[i] += prefixSum[i - 1];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        if (i == 0) return prefixSum[j];
+        else return prefixSum[j] - prefixSum[i - 1];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
