4e2fb8ead8c9034e710b9b4857f19ddc94ce5f19
Makris <vallwesture@gmail.com>
2018-09-20 00:25:34 -0700

    add greedy solution

--- 763_partition_labels.java
+++ 763_partition_labels.java
@@ -1,5 +1,23 @@
 class Solution {
     public List<Integer> partitionLabels(String S) {
+        int[] last = new int[26];
+        for (int i = 0; i < S.length(); i++) {
+            last[S.charAt(i) - 'a'] = i;
+        }
+
+        int j = 0, anchor = 0;
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < S.length(); i++) {
+            j = Math.max(j, last[S.charAt(i) - 'a']);
+            if (i == j) {
+                ans.add(i - anchor + 1);
+                anchor = i + 1;
+            }
+        }
+        return ans;
+    }
+
+    public List<Integer> partitionLabels_myOwn(String S) {
         // define sub problem: start from a letter, and join the remaining
         // ex. given abrjixb -> [a, b, r, j, i, x, b]
         // find the starting index and ending index for each character
