c9c4055ad5fd17a6d0ea03cf4eafccc6a754b2d5
Makris <vallwesture@gmail.com>
2018-10-06 18:11:41 -0700

    add dp solution

--- 139_word_break.java
+++ 139_word_break.java
@@ -1,22 +1,35 @@
 class Solution {
+//     public boolean wordBreak(String s, List<String> wordDict) {
+//         Set<String> wordSet = new HashSet<>(wordDict);
+//         Queue<Integer> queue = new LinkedList<>();
+//         boolean[] visited = new boolean[s.length()];
+//         queue.offer(0);
+
+//         while (! queue.isEmpty()) {
+//             int start = queue.poll();
+//             if (!visited[start]) {
+//                 for (int end = start + 1; end <= s.length(); end++) {
+//                     if (wordSet.contains(s.substring(start, end))) {
+//                         queue.offer(end);
+//                         if (end == s.length()) return true;
+//                     }
+//                 }
+//                 visited[start] = true;
+//             }
+//         }
+//         return false;
+//     }
+
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> wordSet = new HashSet<>(wordDict);
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[s.length()];
-        queue.offer(0);
-
-        while (! queue.isEmpty()) {
-            int start = queue.poll();
-            if (!visited[start]) {
-                for (int end = start + 1; end <= s.length(); end++) {
-                    if (wordSet.contains(s.substring(start, end))) {
-                        queue.offer(end);
-                        if (end == s.length()) return true;
-                    }
-                }
-                visited[start] = true;
+        boolean[] isValid = new boolean[s.length() + 1];
+        isValid[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                isValid[i] = isValid[j] && wordDict.contains(s.substring(j, i));
+                if (isValid[i]) break;
             }
         }
-        return false;
+        return isValid[s.length()];
     }
 }
