42c5f36ef6b1831a86d6e73f9e2ae704680da0ce
Makris Huang <taichiah@gmail.com>
2018-11-07 11:21:01 -0800

    add sol for prob 324 wiggle sort II

---
+++ 324_wiggle_sort_II.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        int len = nums.length;
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+        // handle M S S
+        int lastIdx = (len % 2 == 0 ? len - 2 : len - 1);
+        for (int j = 0, i = lastIdx; i >= 0; i -= 2, j++) {
+            nums[i] = sorted[j];
+        }
+        // handle L L M
+        for (int j = len - 1, i = 1; i < nums.length; i += 2, j--) {
+            nums[i] = sorted[j];
+        }
+    }
+}
