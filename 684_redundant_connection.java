9a89095d10a9a6cf764b965d275b6c47fef38ac8
Makris Huang <taichiah@gmail.com>
2018-11-08 11:25:34 -0800

    modify answer with my style

--- 684_redundant_connection.java
+++ 684_redundant_connection.java
@@ -1,45 +1,52 @@
 class Solution {
-    public int[] findRedundantConnection(int[][] edges) {
 
-        int n = edges.length;
-        DisjointSet ds = new DisjointSet(n);
-
-        for (int[] edge : edges) {
-            int u = edge[0] - 1;
-            int v = edge[1] - 1;
-            if (ds.find(u) == ds.find(v)) {
-                return edge;
-            } else {
-                ds.union(u, v);
-            }
-        }
-        return null;
-
-    }
-
-    class DisjointSet {
-
+    class UnionFind {
         int[] parent;
 
-        public DisjointSet(int N) {
-            parent = new int[N];
-            for (int i = 0; i < N; i++) {
+        public UnionFind(int[][] edges) {
+            Set<Integer> set = new HashSet<>();
+            for (int[] pair: edges) {
+                set.add(pair[0]);
+                set.add(pair[1]);
+            }
+            parent = new int[set.size() + 1];
+            for (int i = 1; i < parent.length; i++) {
                 parent[i] = i;
             }
         }
 
-        public void union(int a, int b) {
-            int rootA = find(a);
-            int rootB = find(b);
-            parent[rootA] = rootB;
+        public void union(int ele1, int ele2) {
+            int root1 = find(ele1);
+            int root2 = find(ele2);
+            parent[root1] = root2;  // ele2 (child) has a far parent, so use it
         }
 
-        public int find(int x) {
-            while (parent[x] != x) {
-                x = parent[x];
+        public int find(int ele) {
+            if (parent[ele] != ele) {
+                parent[ele] = find(parent[ele]);
             }
-            return x;
+            return parent[ele];
         }
 
+        public void print() {
+            for (int par: parent) {
+                System.out.print(par + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int[] findRedundantConnection(int[][] edges) {
+        UnionFind uf = new UnionFind(edges);
+        for (int[] pair: edges) {
+            int child = pair[1];
+            int parent = pair[0];
+            if (uf.find(child) == uf.find(parent)) {
+                return pair;
+            } else {
+                uf.union(parent, child);
+            }
+        }
+        return null;
     }
 }
