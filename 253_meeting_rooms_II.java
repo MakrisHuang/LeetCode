9798504515fab66c38035f074afb5f1cfcc19d77
Makris Huang <tai-chia.huang@sv.cmu.edu>
2018-12-22 00:01:19 -0800

    add code description

--- 253_meeting_rooms_II.java
+++ 253_meeting_rooms_II.java
@@ -12,24 +12,28 @@ class Solution {
         if (intervals == null || intervals.length == 0) return 0;
         // sort them by start time
         List<Interval> intervalList = Arrays.asList(intervals);
-        Collections.sort(intervalList, (interval1, interval2) -> {
+        intervalList.sort((interval1, interval2) -> {
            return interval1.start - interval2.start;
         });
-        
+
+        // use end time to ensure next interval will overpass it
         PriorityQueue<Integer> allocator = new PriorityQueue<Integer>((end1, end2) -> {
-            return end1 - end2; 
+            return end1 - end2;
         });
-        
+
         allocator.offer(intervalList.get(0).end);
-        
+
         for (int i = 1; i < intervalList.size(); i++) {
+            // If the room due to free up the earliest is free, assign that room to this meeting.
             if (intervalList.get(i).start >= allocator.peek()) {
                 allocator.poll();
             }
-            
+
+            // If a new room is to be assigned, then also we add to the heap,
+            // If an old room is allocated, then also we have to add to the heap with updated end time.
             allocator.offer(intervalList.get(i).end);
         }
-        
+
         return allocator.size();
     }
 }
