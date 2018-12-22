beef2a4dfae7f570cfed752e348678393d51c0c8
Makris Huang <tai-chia.huang@sv.cmu.edu>
2018-12-22 00:16:01 -0800

    add more description

--- 418_sentence_screen_fitting.java
+++ 418_sentence_screen_fitting.java
@@ -5,58 +5,33 @@ class Solution {
     // i.e. words in sentence can correctly fit in a row
     // if not, we go back from start index until we have a space
 
+    /*
+        "abc de f abc de f abc de f ..."
+         ^      ^     ^    ^      ^
+         0      7     13   18     25
+    */
+
     // Time Complexity: O(rows + n)
     // Space Complexity: O(n)           given n characters in sentence
     public int wordsTyping(String[] sentence, int rows, int cols) {
         String s = String.join(" ", sentence) + " ";
-        int start = 0, l = s.length();
+        int curr = 0, l = s.length();
         for (int i = 0; i < rows; i++) {
-            start += cols;
-            if (s.charAt(start % l) == ' ') {
-                start++;
+            // directly append a new row
+            curr += cols;
+
+            // If the next position is a space, then this line can fit all words
+            if (s.charAt(curr % l) == ' ') {
+                curr++;
             } else {
                 // if cannot fix, truncate the previous characters
                 // until we meet space again
-                while (start > 0 && s.charAt((start-1) % l) != ' ') {
-                    start--;
+                while (curr > 0 && s.charAt((curr-1) % l) != ' ') {
+                    curr--;
                 }
             }
         }
 
-        return start / s.length();
-
-//         int totalChars = rows * cols;
-//         int startIdx = 0;
-//         int times = 0;
-//         while (startIdx <= totalChars) {
-//             int sentIdx = 0;
-//             while (sentIdx < sentence.length && startIdx <= totalChars) {
-//                 // cound remaing spaces is enought for next word
-//                 System.out.println("word: " + sentence[sentIdx]);
-//                 int remaining = cols - startIdx % cols;
-//                 int wordLen = sentence[sentIdx].length();
-//                 if (remaining > wordLen) {
-//                     startIdx += wordLen;
-//                     startIdx += (remaining == wordLen) ? 0 : 1;  // add space
-
-//                     sentIdx++;
-//                 } else {
-//                     // move to a new line and check validation
-//                     int currRows = (startIdx / cols) + 1;
-//                     System.out.println("curr rows: " + currRows);
-//                     if (currRows + 1 > rows) {  // cannot satisfy all sentences words
-//                         break;
-//                     } else {
-//                         // move to a new line
-//                         startIdx = currRows * cols;
-//                     }
-//                 }
-//                 System.out.println("startIdx: " + startIdx);
-//             }
-//             if (startIdx <= totalChars) {
-//                 times++;
-//             }
-//         }
-//         return times;
+        return curr / s.length();
     }
 }
