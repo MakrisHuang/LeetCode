3fe73fcee84ecd60aff93561f3aa2bdd09bed815
Makris Huang <tai-chia.huang@sv.cmu.edu>
2018-12-22 11:53:03 -0800

    update dfs method

--- 490_the_maze.java
+++ 490_the_maze.java
@@ -1,76 +1,41 @@
 class Solution {
-    int[][] steps = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    // Time Complexity: O(rows * cols (nodes) + (n-1) * (m - 1) (edges))
+    // Space Complexity: O(rows * cols)
+    int[][] directions = new int[][] {
+        {-1, 0},    // up
+        {0, 1},     // right
+        {1, 0},     // down
+        {0, -1}     // left
+    };
 
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        //DFS + cache
-        int rows = maze.length;
-        int cols = maze[0].length;
-        boolean[][] visited = new boolean[rows][cols];
-
-        return dfs(maze, start, destination, visited, rows, cols);
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        return dfs(maze, visited, start[0], start[1], destination);
     }
 
-    private boolean dfs(int[][] maze, int[] start, int[] des, boolean[][] visited, int rows, int cols) {
-        if (visited[start[0]][start[1]]) {
+    public boolean dfs(int[][] maze, boolean[][] visited, int i, int j, int[] dest) {
+        if (visited[i][j]) {
             return false;
         }
-        if (start[0] == des[0] && start[1] == des[1]) {
+
+        if (i == dest[0] && j == dest[1]) {
             return true;
         }
 
-        visited[start[0]][start[1]] = true;
-        for (int[] step : steps) {
-            int dx = start[0];
-            int dy = start[1];
+        visited[i][j] = true;
+        int rows = maze.length, cols = maze[0].length;
+        for (int[] step: directions) {
+            int dx = i;
+            int dy = j;
+            // keep going with the same step to avoid TLE
             while (dx + step[0] >= 0 && dx + step[0] < rows && dy + step[1] >= 0 && dy + step[1] < cols && maze[dx + step[0]][dy + step[1]] != 1) {
                 dx += step[0];
                 dy += step[1];
             }
-            if (dfs(maze, new int[]{dx, dy}, des, visited, rows, cols)) {
+            if (dfs(maze, visited, dx, dy, dest)) {
                 return true;
             }
         }
         return false;
     }
-//     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-//         // go with 4 directions
-//         boolean[][] visited = new boolean[maze.length][maze[0].length];
-//         for (int i = 0; i < maze.length; i++) {
-//             for (int j = 0; j < maze[0].length; j++) {
-//                 if (maze[i][j] == 1) {
-//                     visited[i][j] = true;
-//                 }
-//             }
-//         }
-//         return dfs(maze, visited, start[0], start[1], 0, destionation);
-//     }
-
-//     public boolean dfs(int[][] maze, boolean[][] visited, int x, int y, int prevDir, int[] dest) {
-//         // check wall with prev direction
-//         if (prevDir == 1 && ) { // prev is up
-//         }
-//         int rows = maze.length, cols = maze[0].length;
-
-//         visited[x][y] = true;
-
-//         boolean res = false;
-//         // go up: if up is not at top and up is not a wall
-//         if ((x != 0) && !visited[x - 1][y]) {
-//             res |= dfs(maze, visited, x - 1, y, 1, dest);
-//         }
-//         // go right: if right is not at right and right is not a wall
-//         if ((y != cols - 1) && !visited[x - 1][y]) {
-//             res |= dfs(maze, visited, x, y + 1, 2, dest);
-//         }
-//         // go down: if down is not at bottom and down is not a wall
-//         if ((x != rows - 1) && !visited[x - 1][y]) {
-//             res |= dfs(maze, visited, x - 1, y, 3, dest);
-//         }
-//         // go left: if left is not at left and left is not a wall
-//         if ((y != 0) && !visited[x][y - 1]) {
-//             res |= dfs(maze, visited, x, y - 1, 4, dest);
-//         }
-
-//         return res;
-//     }
 }
