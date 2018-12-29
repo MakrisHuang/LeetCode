2bb93446e6552d184ab45c03af930be52ebac5db
Makris Huang <tai-chia.huang@sv.cmu.edu>
2018-12-28 17:10:22 -0800

    [277] use two pass for solution

--- 277_find_the_celebrity.java
+++ 277_find_the_celebrity.java
@@ -5,8 +5,12 @@ public class Solution extends Relation {
     public int findCelebrity(int n) {
         int celebrity = 0;
         for (int i = 0; i < n; i++) if (knows(celebrity, i)) celebrity = i;
-        for (int i = 0; i < celebrity; i++) if (knows(celebrity, i)) return -1;
-        for (int i = 0; i < n; i++) if (! knows(i, celebrity)) return -1;
+        for (int j = 0; j < n; j++) {
+            if (j == celebrity) continue;
+
+            // if j does not know candidate or candidaet knows j, return -1
+            if ( !knows(j, celebrity) || knows(celebrity, j)) return -1;
+        }
         return celebrity;
     }
 }
