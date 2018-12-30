430d90d38f27f5ffccfd5a4f315de30e2c4520aa
Makris Huang <tai-chia.huang@sv.cmu.edu>
2018-12-29 16:08:51 -0800

    [852] add log(n) solution

--- 852_peak_index_in_a_mountain_array.java
+++ 852_peak_index_in_a_mountain_array.java
@@ -1,9 +1,21 @@
 class Solution {
+    // Time Complexity: O(logn)
+    // Space Complexity: O(1)
     public int peakIndexInMountainArray(int[] A) {
-        int maxIdx = 0;
-        for (int i = 1; i < A.length; i++) {
-            if (A[i] > A[maxIdx]) maxIdx = i;
+        int low = 0, high = A.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (A[mid] < A[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
         }
-        return maxIdx;
+        return low;
+        // int maxIdx = 0;
+        // for (int i = 1; i < A.length; i++) {
+        //     if (A[i] > A[maxIdx]) maxIdx = i;
+        // }
+        // return maxIdx;
     }
 }
