2782a47c723c81d7cb8b05567c672dda06b1c6d4
Makris Huang <tai-chia.huang@sv.cmu.edu>
2018-12-29 22:22:09 -0800

    [080] add description

--- 080_remove_duplicates_from_sorted_array_II.java
+++ 080_remove_duplicates_from_sorted_array_II.java
@@ -5,8 +5,10 @@ class Solution {
             // i < 2: cover lower cases
             // n > nums[i - 2]: if the current num is not equal, then assign current n to nums[i]
             // then increase i
-            if (i < 2 || n > nums[i - 2])
+            if (i < 2 || n > nums[i - 2])   // can be extend to k-times elements => nums[i - k]
                 nums[i++] = n;
+
+            // if above condition cannot be satisfied, move to next n until we find greater element
         }
         return i;
     }
