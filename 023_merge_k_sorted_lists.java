d7eed0c796fb3178899b696cfabe3cbb59ce69d8
Makris Huang <tai-chia.huang@sv.cmu.edu>
2018-12-30 14:24:12 -0800

    [023] add description

--- 023_merge_k_sorted_lists.java
+++ 023_merge_k_sorted_lists.java
@@ -9,22 +9,27 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0) return null;
-        
+
         PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>((node1, node2) -> {
-            return node1.val - node2.val; 
+            return node1.val - node2.val;
         });
-        
+
         for (ListNode node: lists) {
+            // append the first node
             if (node != null) {
-                queue.offer(node);    
+                queue.offer(node);
             }
         }
-        
+
         ListNode head = new ListNode(0);
         ListNode curr = head;
         while (! queue.isEmpty()) {
+            // get the node with smallest value
+            // and link it
             curr.next = queue.poll();
             curr = curr.next;
+
+            // push the remaining node at this linked list
             if (curr.next != null) {
                 queue.offer(curr.next);
             }
