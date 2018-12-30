4d0aa8260481905ad6d42dff070f64276aa83382
Makris Huang <tai-chia.huang@sv.cmu.edu>
2018-12-29 23:42:22 -0800

    [035] add binary search solution

--- 035_search_insert_position.java
+++ 035_search_insert_position.java
@@ -1,13 +1,29 @@
-class Solution {
+iclass Solution {
     public int searchInsert(int[] nums, int target) {
-        int result = -1;
-        for (int i = 0; i < nums.length; i++){
-            if (target <= nums[i]){
-                result = i;
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > target) {
+                high = mid;
+            } else if (nums[mid] < target){
+                low = mid + 1;
+            } else {
+                low = mid;
                 break;
             }
         }
-        if (result == -1) result = nums.length;
-        return result;
+        while (low > 1 && nums[low] == nums[low - 1]) {
+            low--;
+        }
+        return (nums[low] < target) ? nums.length: low;
+        // int result = -1;
+        // for (int i = 0; i < nums.length; i++){
+        //     if (target <= nums[i]){
+        //         result = i;
+        //         break;
+        //     }
+        // }
+        // if (result == -1) result = nums.length;
+        // return result;
     }
 }
