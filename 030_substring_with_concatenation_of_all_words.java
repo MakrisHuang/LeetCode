5b772a19c8e7aaa6da98b0c72e9997c11c3fa35f
Makris Huang <tai-chia.huang@sv.cmu.edu>
2018-12-30 12:58:24 -0800

    [030] simplify answer by removing variables

--- 030_substring_with_concatenation_of_all_words.java
+++ 030_substring_with_concatenation_of_all_words.java
@@ -1,49 +1,46 @@
 class Solution {
-    public List<Integer> findSubstring(String S, String[] L) {
-        List<Integer> res = new LinkedList<>();
-        if (L.length == 0 || S.length() < L.length * L[0].length())   return res;
-        int N = S.length();
-        int M = L.length; // *** length
-        int wl = L[0].length();
-Map<String, Integer> map = new HashMap<>(), curMap = new HashMap<>();
-        for (String s : L) {
-            if (map.containsKey(s))   map.put(s, map.get(s) + 1);
-            else                      map.put(s, 1);
+    // Use two map to store and compare occurrence times
+    // Time Complexity: O(len(s) * wordNum)
+    // Space Complexity: O(wordNum)
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList<>();
+        int wordNum = words.length;
+        if (wordNum == 0) {
+            return res;
         }
-        String str = null, tmp = null;
-        for (int i = 0; i < wl; i++) {
-            int count = 0;  // remark: reset count
-            int start = i;
-            for (int r = i; r + wl <= N; r += wl) {
-                str = S.substring(r, r + wl);
-                if (map.containsKey(str)) {
-                    if (curMap.containsKey(str))   curMap.put(str, curMap.get(str) + 1);
-                    else                           curMap.put(str, 1);
+        int wordLen = words[0].length();
 
-                    if (curMap.get(str) <= map.get(str))    count++;
-                    while (curMap.get(str) > map.get(str)) {
-                        tmp = S.substring(start, start + wl);
-                        curMap.put(tmp, curMap.get(tmp) - 1);
-                        start += wl;
+        Map<String, Integer> allWordsMap = new HashMap<>();
+        for (String word: words) {
+            allWordsMap.put(word, allWordsMap.getOrDefault(word, 0) + 1);
+        }
 
-                        //the same as https://leetcode.com/problems/longest-substring-without-repeating-characters/
-                        if (curMap.get(tmp) < map.get(tmp)) count--;
+        // traverse all substring
+        for (int i = 0; i < s.length() - wordNum * wordLen + 1; i++) {
+            Map<String, Integer> currMap = new HashMap<>();
+            int num = 0;
 
+            // examine the substring is matched
+            while (num < wordNum) {
+                // move to next segment (word)
+                String word = s.substring(i + num * wordLen, i + (num + 1) * wordLen);
+                if (allWordsMap.containsKey(word)) {
+                    currMap.put(word, currMap.getOrDefault(word, 0) + 1);
+
+                    // compare occurrences times
+                    if (currMap.get(word) > allWordsMap.get(word)) {
+                        break;
                     }
-                    if (count == M) {
-                        res.add(start);
-                        tmp = S.substring(start, start + wl);
-                        curMap.put(tmp, curMap.get(tmp) - 1);
-                        start += wl;
-                        count--;
-                    }
-                }else {
-                    curMap.clear();
-                    count = 0;
-                    start = r + wl;//not contain, so move the start
+                } else {
+                    break;
                 }
+                num++;
             }
-            curMap.clear();
+
+            // check number of words we found
+            if (num == wordNum) {
+                res.add(i);
+            }
         }
         return res;
     }
