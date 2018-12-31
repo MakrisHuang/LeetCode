32f7ee76caad09be47d91c28bbf9db8db72055e6
Makris Huang <tai-chia.huang@sv.cmu.edu>
2018-12-31 15:07:02 -0800

    [318] add reason of why using bitmask

--- 318_maximum_product_of_word_lengths.java
+++ 318_maximum_product_of_word_lengths.java
@@ -4,6 +4,7 @@ class Solution {
         int[] nums = new int[len];
         for (int i = 0; i < len; i++) {
             for (char ch: words[i].toCharArray()) {
+                // is using bit to represent if a char exists in the word. For example, if a and b exists, it will be 11 (binary).
                 nums[i] = nums[i] | (1 << (ch -'a'));
             }
         }
