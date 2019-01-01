468275e9dfaba9f0ea3eb68ec1fbf23fadacdc03
Makris Huang <tai-chia.huang@sv.cmu.edu>
2018-12-31 17:34:50 -0800

    [073] add description

--- 073_set_matrix_zeroes.java
+++ 073_set_matrix_zeroes.java
@@ -1,7 +1,8 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         int col0 = 1, rows = matrix.length, cols = matrix[0].length;
-        
+
+        // set first row or first col to 0 if condition is satisfied
         for (int i = 0; i < rows; i++) {
             if (matrix[i][0] == 0) col0 = 0;
             for (int j = 1; j < cols; j++) {
@@ -11,7 +12,8 @@ class Solution {
                 }
             }
         }
-        
+
+        // then set elements to 0 given the first row and first column
         for (int i = rows - 1; i >= 0; i--) {
             for (int j = cols - 1; j >= 1; j--) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
