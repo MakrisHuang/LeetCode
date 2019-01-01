4fde211e020afe0171ce507e861267ad8f599c95
Makris Huang <tai-chia.huang@sv.cmu.edu>
2019-01-01 13:06:03 -0800

    [096] add description for dynamic programming solution

--- 096_unique_binary_search_trees.java
+++ 096_unique_binary_search_trees.java
@@ -1,4 +1,10 @@
 class Solution {
+    // apply dynamic programming: select i as the root, then combine left and right subtree
+    // F(i,n)=G(i−1)⋅G(n−i) where i is the selected root
+    // and we want G(n) = Sigma(G(i - 1) * G(n - i)) where i goes from 1 to n
+
+    // Time complexity: O(n ^ 2)
+    // Space complexity: O(n)
     public int numTrees(int n) {
         int [] dp = new int[n+1];
         dp[0]= 1;
