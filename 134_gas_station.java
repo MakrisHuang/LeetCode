db614de82dd45824c336d832fffc99afe052f477
Makris Huang <vallwesture@gmail.com>
2019-01-01 17:21:40 -0800

    [134] add description for the solution

--- 134_gas_station.java
+++ 134_gas_station.java
@@ -1,5 +1,7 @@
 class Solution {
-    // ???
+    /*
+    The basic idea is every time we start from a station, we go as far as possible by increasing end until remaining gas is less than 0. If 'end' finally hits start we know we can travel around from 'start'. If we haven't traveled around, we know we cannot start from this station. Then we check the station before our start station if we can start from this station. Repeat until we have checked all stations.
+    */
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int len = gas.length;
         int start = len - 1, end = 0;
@@ -13,7 +15,7 @@ class Solution {
                 sum += gas[start] - cost[start];
             }
         }
-        
+
         return (sum >= 0) ? start : -1;
     }
 }
