18f17fc92fb265327c7ea450992e7e24524389d8
Makris Huang <vallwesture@gmail.com>
2019-01-02 21:09:10 -0800

    [237] add description

--- 237_delete_node_in_a_linked_list.java
+++ 237_delete_node_in_a_linked_list.java
@@ -10,6 +10,8 @@ class Solution {
     public void deleteNode(ListNode node) {
         ListNode curr = node;
         ListNode prev = null;
+        // move values (from current to tail) 1 step forward
+        // Time Complexity: O(n);
         while (curr.next != null) {
             ListNode next = curr.next;
             curr.val = next.val;
