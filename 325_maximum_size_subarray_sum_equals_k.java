9ae87ae6d6833753b212962e1735096d4d643a6a
Makris Huang <vallwesture@gmail.com>
2019-01-02 20:11:37 -0800

    [325] add description for put(0, -1) in hashmap

--- 325_maximum_size_subarray_sum_equals_k.java
+++ 325_maximum_size_subarray_sum_equals_k.java
@@ -1,9 +1,12 @@
 class Solution {
     public int maxSubArrayLen(int[] nums, int k) {
         if (nums == null || nums.length == 0) return 0;
-        
+
         int maxLen = 0, sum = 0;
         Map<Integer, Integer> map = new HashMap<>();
+
+        // add the trivial case of 0-length starting subarray to the map
+        // if i == 0 and we found it, then (i - map.get(0)) = (0 - (-1)) = 1 => length = 1
         map.put(0, -1);
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
