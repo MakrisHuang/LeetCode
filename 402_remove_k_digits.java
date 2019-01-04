0636cf20f6cd32d850981afa5e2defe19e797561
Makris Huang <vallwesture@gmail.com>
2019-01-04 15:19:44 -0800

    [402] add complexity analysis

--- 402_remove_k_digits.java
+++ 402_remove_k_digits.java
@@ -1,13 +1,16 @@
 class Solution {
+    // Time Complexity: O(max(len(num), k)), since the inner loop could pop up all elements
+    // given certain k. However, k will decrease while we pop up. So it's based on max(len(num), k)
+    // Space Complexity: O(len(num))
     public String removeKdigits(String num, int k) {
         int len = num.length();
         //corner case
-        if(k==len)
+        if(k == len)
             return "0";
 
         Stack<Character> stack = new Stack<>();
         int i =0;
-        while(i<num.length()){
+        while(i < num.length()){
             //whenever meet a digit which is less than the previous digit, discard the previous one
             while(k>0 && !stack.isEmpty() && stack.peek()>num.charAt(i)){
                 stack.pop();
