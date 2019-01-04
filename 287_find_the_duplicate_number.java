d089da7b7861246aab4c37f95749d53a0e1cf4ca
Makris Huang <vallwesture@gmail.com>
2019-01-04 14:47:05 -0800

    [287] add theory for solution

--- 287_find_the_duplicate_number.java
+++ 287_find_the_duplicate_number.java
@@ -1,11 +1,17 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
+    // Use binary search and pigeonhole theory
+    // ex. nums = [2,3,4,5,6,7,1,1,1,1], low = 1, high = 9, mid = 5
+    // counting num <= 5 is 8, which means there are duplications below 5
+    // so search with (low, mid), otherwise search (mid + 1, hi)
+
+    // not searching index, but value
+    public int finDuplicate(int[] nums) {
         int hi = nums.length - 1, lo = 1;
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
             int count = 0;
             for (int val: nums) {
-                if (val <= mid) {   // compare index ??
+                if (val <= mid) {
                     count++;
                 }
             }
