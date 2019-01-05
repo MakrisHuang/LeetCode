304a4e3d455a517bcaa5533a5f1b864005a4a08e
Makris Huang <vallwesture@gmail.com>
2019-01-04 22:20:39 -0800

    [338] add description for using dynamic programming

--- 338_counting_bits.java
+++ 338_counting_bits.java
@@ -1,15 +1,20 @@
 class Solution {
+    // apply dynamic programming with P(x + b) = P(x) + 2^m where P(i) is the value
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
     public int[] countBits(int num) {
-        int[] res = new int[num + 1];
+        int[] ans = new int[num + 1];
         int i = 0, b = 1;
+        // [0, b) is calculated
         while (b <= num) {
-            while (i < b && i + b <= num) {
-                res[i + b] = res[i] + 1;
-                i++;
+            // generate [b, 2b) or [b, num) from [0, b)
+            while(i < b && i + b <= num){
+                ans[i + b] = ans[i] + 1;
+                ++i;
             }
-            i = 0;
-            b = b << 1;
+            i = 0;   // reset i
+            b <<= 1; // b = 2b
         }
-        return res;
+        return ans;
     }
 }
