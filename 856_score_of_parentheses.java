aafec453bffc7ede9b992354bc6e514bf8dfe84f
Makris Huang <vallwesture@gmail.com>
2019-01-06 11:17:50 -0800

    [856] add description for Math.max()

--- 856_score_of_parentheses.java
+++ 856_score_of_parentheses.java
@@ -9,6 +9,10 @@ class Solution {
             else {
                 int v = stack.pop();
                 int w = stack.pop();
+                // When we see a closing bracket, we add twice the score of the previous deeper part
+
+                // except when counting (), which has a score of 1.
+                // and we can use max(2 * v, 1) for comparison
                 stack.push(w + Math.max(2 * v, 1));
             }
         }
