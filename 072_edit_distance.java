eefb9caeca8e9df69ac28bedd33ca0340ce76a27
Makris Huang <vallwesture@gmail.com>
2019-01-07 14:56:13 -0800

    [072] use dynamic programming solution

--- 072_edit_distance.java
+++ 072_edit_distance.java
@@ -1,20 +1,39 @@
 class Solution {
+    /*
+    There can be three conditions:
+1, word1[i] == word2[j] : then no operation needed. dp[i][j] == dp[i-1][j-1]
+
+2, Do one operation on word1[i-1][j]. dp[i][j] = dp[i-1][j] + 1
+3, Do one operation on word2[i][j-1]. dp[i][j] = dp[i][j-1] + 1
+
+for 2 and 3, the reason it works is that we know the optimal ways to transfrom word1(0,i) to word2(0,j-1) and word1(0,i-1) to word(0,j) ( Delete ("abc" to "ab") or Insert ("ab" to "abc") ). Now all we need to one more operation.
+
+    Time Complexity: O(len(word1) * len(word2))
+    Space Complexity: O(len(word1) * len(word2))
+    */
     public int minDistance(String word1, String word2) {
-        int m = word1.length(), n = word2.length();
-        List<Integer> cur = new ArrayList<>(m + 1);
-        for (int i = 1; i <= m; i++)
-            cur.set(i,i);
-        for (int j = 1; j <= n; j++) {
-            int pre = cur.get(0);
-            cur.set(0,)
-            for (int i = 1; i <= m; i++) {
-                int temp = cur[i];
-                if (word1[i - 1] == word2[j - 1])
-                    cur[i] = pre;
-                else cur[i] = min(pre + 1, min(cur[i] + 1, cur[i - 1] + 1));
-                pre = temp;
+        if (word1.equals(word2)) {
+            return 0;
+        }
+        if (word1.length() == 0 || word2.length() == 0) {
+            return Math.abs(word1.length() - word2.length());
+        }
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i <= word1.length(); i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= word2.length(); i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+                }
             }
         }
-        return cur[m]; 
+        return dp[word1.length()][word2.length()];
     }
 }
