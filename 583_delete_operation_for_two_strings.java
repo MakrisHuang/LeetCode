940dd260bf039e540d7fbeee3ec07726a5bf6d3f
Makris Huang <vallwesture@gmail.com>
2019-01-07 00:25:04 -0800

    [583] use longest common subsequences with memorization

--- 583_delete_operation_for_two_strings.java
+++ 583_delete_operation_for_two_strings.java
@@ -1,19 +1,21 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
-        // apply longest common subsequence
-        // the minDistance = len(word1) + len(word2) - 2 * len(lcs)
-        int len1 = word1.length();
-        int len2 = word2.length();
-        int[][] dp = new int[len1 + 1][len2 + 1];
-        for (int i = 1; i <= len1; i++) {
-            for (int j = 1; j <= len2; j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-        return len1 + len2 - 2 * dp[len1][len2];
+    // apply longest common subsequence
+    // the minDistance = len(word1) + len(word2) - 2 * len(lcs)
+    // Time Complexity : O(len(word1) * len(word2))
+    // Space Complexity: O(len(word1) * len(word2))
+    public int minDistance(String s1, String s2) {
+        int[][] memo = new int[s1.length() + 1][s2.length() + 1];
+        return s1.length() + s2.length() - 2 * lcs(s1, s2, s1.length(), s2.length(), memo);
+    }
+    public int lcs(String s1, String s2, int m, int n, int[][] memo) {
+        if (m == 0 || n == 0)
+            return 0;
+        if (memo[m][n] > 0)
+            return memo[m][n];
+        if (s1.charAt(m - 1) == s2.charAt(n - 1))
+            memo[m][n] = 1 + lcs(s1, s2, m - 1, n - 1, memo);
+        else
+            memo[m][n] = Math.max(lcs(s1, s2, m, n - 1, memo), lcs(s1, s2, m - 1, n, memo));
+        return memo[m][n];
     }
 }
