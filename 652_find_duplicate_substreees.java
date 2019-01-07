02f28098a2953e60f79c963dc8bc44b3f6b4a209
Makris Huang <vallwesture@gmail.com>
2019-01-06 18:25:56 -0800

    [652] use simplified version by serializing tree node

--- 652_find_duplicate_substreees.java
+++ 652_find_duplicate_substreees.java
@@ -8,46 +8,22 @@
  * }
  */
 class Solution {
-    int t;
-    Map<String, Integer> trees;
-    Map<Integer, Integer> count;
-    List<TreeNode> ans;
-
+    Map<String, Integer> map;
+    List<TreeNode> res;
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        t = 1;
-        trees = new HashMap();
-        count = new HashMap();
-        ans = new ArrayList();
-        lookup(root);
-        return ans;
+        map = new HashMap<>();
+        res = new ArrayList<>();
+        serialize(root);
+        return res;
     }
 
-    public int lookup(TreeNode node) {
-        if (node == null) return 0;
-        String serial = node.val + "," + lookup(node.left) + "," + lookup(node.right);
-        int uid = trees.computeIfAbsent(serial, x-> t++);
-        count.put(uid, count.getOrDefault(uid, 0) + 1);
-        if (count.get(uid) == 2)
-            ans.add(node);
-        return uid;
+    public String serialize(TreeNode root) {
+        if (root == null) return "#";
+        String str = root.val + "," + serialize(root.left) + "," + serialize(root.right);
+        map.put(str, map.getOrDefault(str, 0) + 1);
+        if (map.get(str) == 2) {
+            res.add(root);
+        }
+        return str;
     }
-
-//     Map<String, Integer> map;
-//     List<TreeNode> res;
-//     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-//         map = new HashMap<>();
-//         res = new ArrayList<>();
-//         serialize(root);
-//         return res;
-//     }
-
-//     private String serialize(TreeNode node) {
-//         if (node == null) return "#";
-//         String serializedStr = node.val + "," + serialize(node.left) + "," + serialize(node.right);
-//         map.put(serializedStr, map.getOrDefault(serializedStr, 0) + 1);
-//         if (map.get(serializedStr) == 2) {
-//             res.add(node);
-//         }
-//         return serializedStr;
-//     }
 }
