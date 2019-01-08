66cabc19198dd5b5d88403dfabc5652897ca7053
Makris Huang <vallwesture@gmail.com>
2019-01-07 17:02:18 -0800

    [189] add logs for demonstration

--- 189_rotate_array.java
+++ 189_rotate_array.java
@@ -1,32 +1,41 @@
-class Solution {
-//     public void rotate(int[] nums, int k) {
-//         int len = nums.length;
-//         k = k % len;
-//         if (k == 0) return;
-
-//         for (int moveIdx = len - k, origIdx = 0; moveIdx < len; moveIdx++, origIdx++) {
-//             int temp = nums[moveIdx];
-//             for (int i = moveIdx - 1; i >= origIdx; i--) {
-//                 nums[i + 1] = nums[i];
-//             }
-//             nums[origIdx] = temp;
-//         }
-//     }
-
+class Solution {    
+    /*
+        given example: [1,2,3,4,5,6,7] and k = 3
+        output: 
+            1 2 3 1 5 6 7 
+            1 2 3 1 5 6 4 
+            1 2 7 1 5 6 4 
+            1 2 7 1 5 3 4 
+            1 6 7 1 5 3 4 
+            1 6 7 1 2 3 4 
+            5 6 7 1 2 3 4 
+    */
     public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return;
+        
         k %= nums.length;
-        reverse(nums, 0, nums.length - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
-    }
-
-    public void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int currIdx = start;
+            int prev = nums[start];
+            do {
+                int nextIdx = (currIdx + k) % nums.length;
+                // System.out.println("nextIdx: " + nextIdx);
+                int temp = nums[nextIdx];
+                nums[nextIdx] = prev;
+                prev = temp;
+                currIdx = nextIdx;
+                // System.out.println("currIdx: " + currIdx);
+                count++;
+                print(nums);
+            } while (start != currIdx);
         }
+    }
+    
+    public void print(int[] nums){
+        for (int val: nums) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
     }
 }
