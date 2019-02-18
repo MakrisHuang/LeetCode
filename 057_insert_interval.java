5688196b70581da8ab7cb1b8b6181d2c15abd49b
taichiah <vallwesture@gmail.com>
2019-02-18 00:30:21 -0800

    [057] change solution with direct comparison

--- 057_insert_interval.java
+++ 057_insert_interval.java
@@ -8,53 +8,76 @@
  * }
  */
 class Solution {
+    // Time Complexity: O(n)
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-        List<Interval> part = new ArrayList<>();
-        List<Integer> mid = new ArrayList<>();
+        List<Interval> result = new ArrayList<>();
+        int i = 0;
+        int start = newInterval.start;
+        int end = newInterval.end;
 
-        for (int i = 0; i < intervals.size(); i++) {
-            Interval iv = intervals.get(i);
-            if ((iv.end >= newInterval.start && iv.start <= newInterval.start) ||
-                (iv.end >= newInterval.end && iv.start <= newInterval.end) ||
-                (iv.start >= newInterval.start && iv.end <= newInterval.end)) {
-                part.add(iv);
-                mid.add(i);
-            }
+
+        while (i < intervals.size() && intervals.get(i).end < start) {
+            result.add(intervals.get(i++));
         }
 
-        if (part.size() == 0) {
-            // no overlapping, find the right position to insert the interval
-            for (int i = 0; i < intervals.size(); i++) {
-                if (i == 0 && newInterval.start < intervals.get(i).start) {
-                    intervals.add(0, newInterval);
-                    return intervals;
-                } else if (i > 0 && newInterval.start < intervals.get(i).start &&
-                    newInterval.start > intervals.get(i - 1).start) {
-                    intervals.add(i, newInterval);
-                    return intervals;
-                }
-            }
-            intervals.add(newInterval);
-            return intervals;
-
-        } else {
-            List<Interval> res = new ArrayList<>();
-            part.add(newInterval);
-            List<Interval> merged = merge(part);
-
-            // copy the front part
-            for (int i = 0; i < mid.get(0); i++) {
-                res.add(intervals.get(i));
-            }
-
-            res.addAll(merged);
-
-            // copy the back part
-            for (int j = mid.get(mid.size() - 1) + 1; j < intervals.size(); j++) {
-                res.add(intervals.get(j));
-            }
-            return res;
+        // find interval in the middle of list
+        // where the new interval is shorter in end, compared to start from current interval
+        while (i < intervals.size() && intervals.get(i).start <= end) {
+            start = Math.min(start, intervals.get(i).start);
+            end = Math.max(end, intervals.get(i).end);
+            i++;
         }
+        result.add(new Interval(start,end));
+
+        while (i < intervals.size()) result.add(intervals.get(i++));
+        return result;
+
+//         List<Interval> part = new ArrayList<>();
+//         List<Integer> mid = new ArrayList<>();
+
+//         for (int i = 0; i < intervals.size(); i++) {
+//             Interval iv = intervals.get(i);
+//             if ((iv.end >= newInterval.start && iv.start <= newInterval.start) ||
+//                 (iv.end >= newInterval.end && iv.start <= newInterval.end) ||
+//                 (iv.start >= newInterval.start && iv.end <= newInterval.end)) {
+//                 part.add(iv);
+//                 mid.add(i);
+//             }
+//         }
+
+//         if (part.size() == 0) {
+//             // no overlapping, find the right position to insert the interval
+//             for (int i = 0; i < intervals.size(); i++) {
+//                 if (i == 0 && newInterval.start < intervals.get(i).start) {
+//                     intervals.add(0, newInterval);
+//                     return intervals;
+//                 } else if (i > 0 && newInterval.start < intervals.get(i).start &&
+//                     newInterval.start > intervals.get(i - 1).start) {
+//                     intervals.add(i, newInterval);
+//                     return intervals;
+//                 }
+//             }
+//             intervals.add(newInterval);
+//             return intervals;
+
+//         } else {
+//             List<Interval> res = new ArrayList<>();
+//             part.add(newInterval);
+//             List<Interval> merged = merge(part);
+
+//             // copy the front part
+//             for (int i = 0; i < mid.get(0); i++) {
+//                 res.add(intervals.get(i));
+//             }
+
+//             res.addAll(merged);
+
+//             // copy the back part
+//             for (int j = mid.get(mid.size() - 1) + 1; j < intervals.size(); j++) {
+//                 res.add(intervals.get(j));
+//             }
+//             return res;
+//         }
     }
 
     public List<Interval> merge(List<Interval> intervals) {
