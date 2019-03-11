e445b325f264b7c1136848dfadf78dc68e5d7191
taichiah <vallwesture@gmail.com>
2019-03-11 11:07:56 -0700

    use array variable for changing direction

--- 688_knight_probability_in_chessboard.java
+++ 688_knight_probability_in_chessboard.java
@@ -4,37 +4,22 @@ class Solution {
     public double knightProbability(int N, int K, int r, int c) {
         double moves = 0;
         double[][][] dp = new double[N][N][K + 1];
+        int[] dr = new int[] {-2, -1, 1, 2, 2, 1, -1, -2};
+        int[] dc = new int[] {1, 2, 2, 1, -1, -2, -2, -1};
+
         // initialization
         dp[r][c][0] = 1.0;
 
         for (int step = 1; step <= K; step++) {
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (i - 2 >= 0 && j + 1 < N) {
-                        dp[i][j][step] += dp[i - 2][j + 1][step - 1] / 8;
+                    for (int d = 0; d < 8; d++) {
+                        int cr = i + dr[d];
+                        int cc = j + dc[d];
+                        if (cr >= 0 && cr < N && cc >= 0 && cc < N) {
+                            dp[i][j][step] += dp[cr][cc][step - 1] / 8;
+                        }
                     }
-                    if (i - 1 >= 0 && j + 2 < N) {
-                        dp[i][j][step] += dp[i - 1][j + 2][step - 1] / 8;
-                    }
-                    if (i + 1 < N && j + 2 < N) {
-                        dp[i][j][step] += dp[i + 1][j + 2][step - 1] / 8;
-                    }
-                    if (i + 2 < N && j + 1 < N) {
-                        dp[i][j][step] += dp[i + 2][j + 1][step - 1] / 8;
-                    }
-                    if (i + 2 < N && j - 1 >= 0) {
-                        dp[i][j][step] += dp[i + 2][j - 1][step - 1] / 8;
-                    }
-                    if (i + 1 < N && j - 2 >= 0) {
-                        dp[i][j][step] += dp[i + 1][j - 2][step - 1] / 8;
-                    }
-                    if (i - 1 >= 0 && j - 2 >= 0) {
-                        dp[i][j][step] += dp[i - 1][j - 2][step - 1] / 8;
-                    }
-                    if (i - 2 >= 0 && j - 1 >= 0) {
-                        dp[i][j][step] += dp[i - 2][j - 1][step - 1] / 8;
-                    }
-                    // System.out.println("dp[" + i + "][" + j + "][" + step + "] = " + dp[i][j][step]);
                 }
             }
         }
