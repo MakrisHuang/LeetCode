fcd342c6966b74f5d5b3137176aaee50853a0be6
Makris Huang <vallwesture@gmail.com>
2019-05-05 11:41:22 -0700

    add comment

--- 063_unique_paths_II.java
+++ 063_unique_paths_II.java
@@ -1,17 +1,18 @@
 class Solution {
-    // use dynamic programming, bottom-up algorithm to calculating results
+    // Bottom-Up approach
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        if (m == 0 || obstacleGrid[0].length == 0) return 0;
-        int n = obstacleGrid[0].length;
-
-        int[][] mat = new int[m + 1][n + 1];
-        mat[m - 1][n] = 1;
-        for (int r = m - 1; r >= 0; r--) {
-            for (int c = n - 1; c >= 0; c--) {
-                mat[r][c] = (obstacleGrid[r][c] == 1) ? 0 : mat[r + 1][c] + mat[r][c + 1];
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+        dp[m][n - 1] = 1; // either dp[m - 1][n] is fine
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+                } else {
+                    dp[i][j] = 0;
+                }
             }
         }
-        return mat[0][0];
+        return dp[0][0];
     }
 }
