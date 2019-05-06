bbc397aea7ce1e9227d6f422382a51c88a8bd4bc
Makris Huang <vallwesture@gmail.com>
2019-05-05 18:31:14 -0700

    update solution with a more structured style

--- 639_decode_ways_II.java
+++ 639_decode_ways_II.java
@@ -1,32 +1,49 @@
 class Solution {
     public int numDecodings(String s) {
-        long M = 1000000007;
-        
         long[] dp = new long[s.length() + 1];
         dp[0] = 1;
-        dp[1] = (s.charAt(0) == '*') ? 9 : (s.charAt(0) == '0') ? 0 : 1;
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == '*') {
-                dp[i + 1] = 9 * dp[i];
-                if (s.charAt(i - 1) == '1')
-                    dp[i + 1] = (dp[i + 1] + 9 * dp[i - 1]) % M;
-                else if (s.charAt(i - 1) == '2')
-                    // consider 21 - 26
-                    dp[i + 1] = (dp[i + 1] + 6 * dp[i - 1]) % M;
-                else if (s.charAt(i - 1) == '*')
-                    // consider 11 - 19 and 21 - 26
-                    dp[i + 1] = (dp[i + 1] + 15 * dp[i - 1]) % M;
-            } else {
-                // remember handle invalid cases
-                dp[i + 1] = s.charAt(i) != '0' ? dp[i] : 0;
-                if (s.charAt(i - 1) == '1')
-                    dp[i + 1] = (dp[i + 1] + dp[i - 1]) % M;
-                else if (s.charAt(i - 1) == '2' && s.charAt(i) <= '6')
-                    dp[i + 1] = (dp[i + 1] + dp[i - 1]) % M;
-                else if (s.charAt(i - 1) == '*')
-                    dp[i + 1] = (dp[i + 1] + (s.charAt(i) <= '6' ? 2 : 1) * dp[i - 1]) % M;
-            }
+        if (s.charAt(0) == '0') {
+            return 0;
         }
-        return (int)dp[s.length()];
+        dp[1] = (s.charAt(0) == '*') ? 9 : 1;
+
+        // buttom up
+        for (int i = 2; i <= s.length(); i++) {
+            char first = s.charAt(i - 2);
+            char second = s.charAt(i - 1);
+
+            // for dp[i - 1]
+            if (second == '*') {
+                dp[i] += 9 * dp[i - 1];
+            } else if (second > '0') {
+                dp[i] += dp[i - 1];
+            }
+
+            // for dp[i - 2]
+            if (first == '1' || first == '2') {
+                if (second == '*') {
+                    if (first == '1') {
+                        dp[i] += 9 * dp[i - 2];
+                    } else if (first == '2') {
+                        dp[i] += 6 * dp[i - 2];
+                    }
+                } else if (second != '*') {
+                    if (Integer.valueOf(s.substring(i - 2, i)) <= 26) {
+                        dp[i] += dp[i - 2];
+                    }
+                }
+            } else if (first == '*') {
+                if (second == '*') {
+                    dp[i] += 15 * dp[i - 2];
+                } else if (second <= '6') {
+                    dp[i] += 2 * dp[i - 2];
+                } else {
+                    dp[i] += dp[i - 2];
+                }
+            }
+
+            dp[i] %= 1000000007;
+        }
+        return (int) dp[s.length()];
     }
 }
