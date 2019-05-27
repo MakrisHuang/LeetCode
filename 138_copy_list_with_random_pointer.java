a8c1c1c84e24018ec53ce2d0352f1c659f6da5b4
Makris Huang <vallwesture@gmail.com>
2019-05-27 09:03:14 +0800

    add explanation for 138

--- 138_copy_list_with_random_pointer.java
+++ 138_copy_list_with_random_pointer.java
@@ -1,31 +1,40 @@
-/**
- * Definition for singly-linked list with a random pointer.
- * class RandomListNode {
- *     int label;
- *     RandomListNode next, random;
- *     RandomListNode(int x) { this.label = x; }
- * };
- */
-public class Solution {
-    public RandomListNode copyRandomList(RandomListNode head) {
-        Map<RandomListNode, RandomListNode> map = new HashMap<>();
-        RandomListNode dummy = new RandomListNode(0);
-        RandomListNode q = dummy;
-        dummy.next = head;
-        RandomListNode curr = head;
-        while (curr != null){
-            q.next = new RandomListNode(curr.label);
-            map.put(curr, q.next);
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node() {}
+
+    public Node(int _val,Node _next,Node _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+};
+*/
+class Solution {
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> randomMap = new HashMap<>();
+        Node dummy = new Node();
+        Node p = dummy;
+        Node q = head;
+        while (q != null) {
+            p.next = new Node(q.val, null, null);
+
+            randomMap.put(q, p.next);   // <original node, deep-copied node> ex. {1 -> 1}, {2 -> 2}
+
+            p = p.next;
             q = q.next;
-            curr = curr.next;
         }
-        // move to head
-        curr = head;
-        q = dummy;
-        while (curr != null){
-            q.next.random = map.get(curr.random);
+        p = dummy;
+        q = head;
+        while (q != null) {
+            // q.random represents original random node, and queried value is the deep-copied node
+            p.next.random = randomMap.get(q.random);
+            p = p.next;
             q = q.next;
-            curr = curr.next;
         }
         return dummy.next;
     }
