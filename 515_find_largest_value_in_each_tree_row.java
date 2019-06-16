3c7a57856576e72691b75c4aee860d7de6336fab
Makris Huang <vallwesture@gmail.com>
2019-06-15 23:55:35 -0400

    use bfs for prob 515 find largest value in each tree row

--- 515_find_largest_value_in_each_tree_row.java
+++ 515_find_largest_value_in_each_tree_row.java
@@ -9,44 +9,35 @@
  */
 class Solution {
     public List<Integer> largestValues(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        visit(root, 0, result);
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
 
-        List<Integer> largest = new ArrayList<>();
-        for (int row = 0; row < result.size(); row++) {
-            List<Integer> layer = result.get(row);
-            int max = layer.get(0);
-            for (int i = 1; i < layer.size(); i++) {
-                if (layer.get(i) > max) max = layer.get(i);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        q.offer(null);
+
+        int largest = Integer.MIN_VALUE;
+        while (q.size() > 0) {
+            TreeNode curr = q.poll();
+            if (curr != null) {
+                if (curr.val > largest) {
+                    largest = curr.val;
+                }
+                if (curr.left != null) {
+                    q.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    q.offer(curr.right);
+                }
+            } else {
+                res.add(largest);
+                largest = Integer.MIN_VALUE;
+                if (q.size() > 0) {
+                    q.offer(null);
+                }
             }
-
-            largest.add(max);
         }
-        return largest;
-    }
 
-    // Time Complexity: O(n), Space Complexity: O(n)
-    private void visit(TreeNode node, int level, List<List<Integer>> result) {
-        if (node == null) return;
-        else {
-            if (level >= result.size()) {
-                result.add(new ArrayList<>());
-            }
-            List<Integer> layer = result.get(level);
-
-            layer.add(node.val);
-            level++;
-            visit(node.left, level, result);
-            visit(node.right, level, result);
-        }
-    }
-
-    private void print(List<List<Integer>> list) {
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = 0; j < list.get(i).size(); j++) {
-                System.out.print(list.get(i).get(j) + " ");
-            }
-            System.out.println();
-        }
+        return res;
     }
 }
