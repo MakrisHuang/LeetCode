1b5860c4414e0ac9a0e251d76b0d6cdbd7e084e4
Makris Huang <vallwesture@gmail.com>
2019-08-10 12:50:21 -0400

    use linked list to remove duplicated

---
+++ python/083_remove_duplicates_from_sorted_list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        res = head
+        while head:
+            next = head.next
+            while next and next.val == head.val:
+                next = next.next
+            head.next = next
+            head = next
+        return res
