1b2fce929f2271e90629aeae92369e8c322e9b1d
Makris Huang <vallwesture@gmail.com>
2019-08-11 10:45:44 -0400

    add iterative way

--- python/104_maximum_depth_binary_tree.py
+++ python/104_maximum_depth_binary_tree.py
@@ -6,12 +6,26 @@
 #         self.right = None
 
 class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        return self.findMax(root, 0)
+#     def maxDepth(self, root: TreeNode) -> int:
+#         return self.findMax(root, 0)
 
-    def findMax(self, node: TreeNode, depth: int) -> int:
-        if not node:
-            return depth
-        left_depth = self.findMax(node.left, depth + 1)
-        right_depth = self.findMax(node.right, depth + 1)
-        return max(left_depth, right_depth)
+#     def findMax(self, node: TreeNode, depth: int) -> int:
+#         if not node:
+#             return depth
+#         left_depth = self.findMax(node.left, depth + 1)
+#         right_depth = self.findMax(node.right, depth + 1)
+#         return max(left_depth, right_depth)
+
+    def maxDepth(self, root: TreeNode) -> int:
+        stack = []
+        if root is not None:
+            stack.append((1, root))
+        depth = 0
+        while stack != []:
+            current_depth, root = stack.pop()
+            if root is not None:
+                depth = max(current_depth, depth)
+                stack.append((current_depth + 1, root.left))
+                stack.append((current_depth + 1, root.right))
+        return depth
+
