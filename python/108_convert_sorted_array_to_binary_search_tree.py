1c5eea949bf83c9be885f1bb017030f771b13120
Makris Huang <vallwesture@gmail.com>
2019-08-11 01:57:02 -0400

    use recursion to construct tree

---
+++ python/108_convert_sorted_array_to_binary_search_tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if len(nums) == 0:
+            return None
+        mid = int(len(nums) / 2)
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[0:mid])
+        node.right = self.sortedArrayToBST(nums[mid + 1:])
+        return node
+
