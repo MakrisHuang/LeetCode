150a784467b0125e613000623eb9ad0ea61c7c22
Makris Huang <vallwesture@gmail.com>
2019-08-11 02:02:08 -0400

    add using index version

--- python/108_convert_sorted_array_to_binary_search_tree.py
+++ python/108_convert_sorted_array_to_binary_search_tree.py
@@ -15,3 +15,17 @@ class Solution:
         node.right = self.sortedArrayToBST(nums[mid + 1:])
         return node
 
+    """
+    # pass index to avoid costs in slicing array
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+            def convert(left, right):
+            if left > right:
+                return None
+            mid = (left + right) // 2
+            node = TreeNode(nums[mid])
+            node.left = convert(left, mid - 1)
+            node.right = convert(mid + 1, right)
+            return node
+        return convert(0, len(nums) - 1)
+    """
+
