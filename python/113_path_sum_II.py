ec076a779a7d1d6da5e0cecd82e1e432444b62d9
Makris Huang <vallwesture@gmail.com>
2019-08-28 21:40:23 -0700

    use recursion

---
+++ python/113_path_sum_II.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        res = []
+        self.findPath(root, sum, res, [])
+        return [list(t) for t in res]
+
+    def findPath(self, root: TreeNode, sum: int, paths: List[List[int]], temp: List[int]):
+        if not root: return
+
+        sum -= root.val
+        temp.append(root.val)
+
+        if not root.left and not root.right:
+            if sum == 0:
+                paths.append(list(temp))
+
+        self.findPath(root.left, sum, paths, temp)
+        self.findPath(root.right, sum, paths, temp)
+        temp.pop()
