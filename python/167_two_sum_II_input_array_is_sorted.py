759a26de3225e40ee5e047bb14bef2ed56c31aca
Makris Huang <vallwesture@gmail.com>
2019-09-03 11:28:50 -0700

    apply binary search for prob 167 two sum input array is sorted

--- python/167_two_sum_II_input_array_is_sorted.py
+++ python/167_two_sum_II_input_array_is_sorted.py
@@ -1,7 +1,20 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        seen = {}
-        for i, val in enumerate(numbers):
-            if target - val in seen:
-                return [seen[target - val], i + 1]
-            seen[val] = i + 1
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # apply binary search
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid_idx = left + (right - left) // 2
+            if nums[left] + nums[right] < target:
+                if nums[mid_idx] + nums[right] < target:
+                    left = mid_idx + 1
+                else:
+                    left += 1
+            elif nums[left] + nums[right] > target:
+                if nums[mid_idx] + nums[left] > target:
+                    right = mid_idx
+                else:
+                    right -= 1
+            else:
+                return [left + 1, right + 1]
+        return []
