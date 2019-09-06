25dfa9f06e879288958b12fdc46c4c9e884bda99
Makris Huang <vallwesture@gmail.com>
2019-09-06 14:43:16 -0700

    using two pointer for prob 392 is subsequence

---
+++ python/392_is_subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        t_idx = s_idx = 0
+        while s_idx < len(s) and t_idx < len(t):
+            if s[s_idx] == t[t_idx]:
+                s_idx += 1
+                t_idx += 1
+            else:
+                t_idx += 1
+        return s_idx == len(s)
