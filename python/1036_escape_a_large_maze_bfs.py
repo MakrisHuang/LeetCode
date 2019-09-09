bf2780690037cdda2ce95b464b2d643d03abc5e5
Makris Huang <vallwesture@gmail.com>
2019-09-09 11:28:01 -0700

    add description

--- python/1036_escape_a_large_maze_bfs.py
+++ python/1036_escape_a_large_maze_bfs.py
@@ -1,15 +1,5 @@
 class Solution:
-    """
-    Time Complexity: O(len(blocked) ^ 2)
-    Space Complexity: O(len(blocked) ^ 2)
-    """
     def isEscapePossible(self, blocked: List[List[int]], source: List[int], target: List[int]) -> bool:
-        """
-        :type blocked: List[List[int]]
-        :type source: List[int]
-        :type target: List[int]
-        :rtype: bool
-        """
         if not blocked: return True
         blocked = set(map(tuple, blocked))
 
@@ -17,16 +7,19 @@ class Solution:
             si, sj = source
             ti, tj = target
             level = 0
-            q = collections.deque([(si,sj)])
-            vis = set()
+            q = collections.deque([(si, sj)])
+            visited = set()
             while q:
                 for _ in range(len(q)):
-                    i,j = q.popleft()
+                    i, j = q.popleft()
                     if i == ti and j == tj: return True
-                    for x,y in ((i+1,j),(i-1,j),(i,j+1),(i,j-1)):
-                        if 0 <= x < 10**6 and 0 <= y < 10**6 and (x,y) not in vis and (x,y) not in blocked:
-                            vis.add((x,y))
-                            q.append((x,y))
+                    for x, y in ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)):
+                        if 0 <= x < 10**6 and 0 <= y < 10**6 and (x, y) not in visited and (x, y) not in blocked:
+                            visited.add((x, y))
+                            q.append((x, y))
+                # if we found all nodes are visited, and number of nodes visted equals to
+                # length of blocked, indicating we are consuming all nodes in this area
+                # so we are able to go out this area to find out target node
                 level += 1
                 if level == len(blocked): break
             else:
