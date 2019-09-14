ea2d57985a0ee19392586425faeb62e17cd9ca50
Makris Huang <vallwesture@gmail.com>
2019-09-14 10:12:01 -0700

    add sol for prob 860 lemonade change

---
+++ python/860_lemonade_change.py
@@ -0,0 +1,27 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        bill_5 = bill_10 = bill_20 = 0
+        for bill in bills:
+            if bill == 5:
+                bill_5 += 1
+            elif bill == 10:
+                if bill_5 == 0:
+                    return False
+                else:
+                    bill_10 += 1
+                    bill_5 -= 1
+            elif bill == 20:
+                if bill_5 == 0:
+                    return False
+                else:
+                    # try 10 + 5 first
+                    if bill_10 > 0:
+                        bill_10 -= 1
+                        bill_5 -= 1
+                    elif bill_5 > 3:
+                        bill_5 -= 3
+                    else:
+                        return False
+                    bill_20 += 1
+        return True
+
