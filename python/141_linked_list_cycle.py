35c9fbb9bb68280dee4c5ca0c2d1917358387304
Makris Huang <vallwesture@gmail.com>
2019-09-29 17:59:05 -0700

    add sol for prob 141 linkned list cycle

---
+++ python/141_linked_list_cycle.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    def hasCycle(self, head: ListNode) -> bool:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
