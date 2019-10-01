ecf6e29835938e3708b5c09785071fdb511320df
Makris Huang <vallwesture@gmail.com>
2019-09-30 19:14:55 -0700

    update time complexity

--- python/787_cheapest_flights_within_k_stops.py
+++ python/787_cheapest_flights_within_k_stops.py
@@ -1,20 +1,25 @@
-class Solution(object):
-    def findCheapestPrice(self, n, flights, src, dst, K):
+from heapq import heappush, heappop
+class Solution:
+    # Time Complexity: O(ElogE) (logE: for heap doing heapify, E: for iterating all edges)
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
         graph = collections.defaultdict(dict)
         for u, v, w in flights:
             graph[u][v] = w
 
-        best = {}
-        pq = [(0, 0, src)]
-        while pq:
-            cost, k, place = heapq.heappop(pq)
+        best = dict()
+        q = [(0, 0, src)]   # cost, k, city
+        while q:
+            cost, k, place = heappop(q)
             if k > K + 1: continue
             if place == dst: return cost
 
             for nei, wt in graph[place].items():
                 newcost = cost + wt
                 if newcost < best.get((k + 1, nei), float('inf')):
-                    heapq.heappush(pq, (newcost, k+1, nei))
-                    best[k + 1, nei] = newcost
-
+                    heappush(q, (newcost, k + 1, nei))
+                    best[(k + 1), nei] = newcost
         return -1
+
+
+
+
