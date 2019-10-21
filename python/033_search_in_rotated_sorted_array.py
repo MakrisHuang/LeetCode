d5fb65e776469ed211f31c1c21017eb36451e17e
Makris Huang <vallwesture@gmail.com>
2019-10-21 01:14:04 -0700

    use faster method with O(logn) for prob 033 search in rotated sorted array

--- python/033_search_in_rotated_sorted_array.py
+++ python/033_search_in_rotated_sorted_array.py
@@ -1,24 +1,51 @@
 class Solution:
+    # find out the break point and search of either left or right part
+    # Time Complexity: O(logn)
+    # Space Complexity: O(1)
     def search(self, nums: List[int], target: int) -> int:
-        offset = 0
-        for i in range(1, len(nums)):
-            if nums[i] < nums[offset]:
-                offset = i
-                break
+        def find_rotate_index(left, right):
+            if nums[left] < nums[right]:
+                return 0
 
-        nums.sort()
-        lo, hi = 0, len(nums) - 1
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            if nums[mid] > target:
-                hi = mid - 1
-            elif nums[mid] < target:
-                lo = mid + 1
-            else:
-                index = mid - (len(nums) - offset)
-                if index < 0:
-                    index = len(nums) + index
-                return index
-        return -1
+            while left <= right:
+                pivot = (left + right) // 2
+                if nums[pivot] > nums[pivot + 1]:
+                    return pivot + 1
+                else:
+                    if nums[pivot] < nums[left]:
+                        right = pivot - 1
+                    else:
+                        left = pivot + 1
 
+        def search(left, right):
+            # binary search
+            while left <= right:
+                pivot = (left + right) // 2
+                if nums[pivot] == target:
+                    return pivot
+                else:
+                    if target < nums[pivot]:
+                        right = pivot - 1
+                    else:
+                        left = pivot + 1
+            return -1
 
+        n = len(nums)
+        if n == 0:
+            return -1
+        if n == 1:
+            return 0 if nums[0] == target else -1
+
+        rotate_index = find_rotate_index(0, n - 1)
+
+        # if target is the smallest element
+        if nums[rotate_index] == target:
+            return rotate_index
+        # if array is not rotated, search in the entire array
+        if rotate_index == 0:
+            return search(0, n - 1)
+        if target < nums[0]:
+            # search on the right side
+            return search(rotate_index, n - 1)
+        # search on the left side
+        return search(0, rotate_index)
