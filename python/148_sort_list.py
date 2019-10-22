71c40155e686ada6b76fb7a7ac4c498b51bce7d0
Makris Huang <vallwesture@gmail.com>
2019-10-22 14:33:49 -0700

    update function description for prob 148 sort list

--- python/148_sort_list.py
+++ python/148_sort_list.py
@@ -5,57 +5,61 @@
 #         self.next = None
 
 class Solution:
-    def sortList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        if head is None: return None
-
-        def getSize(head):
+    def sortList(self, head: ListNode) -> ListNode:
+        def getSize(head) -> int:
             counter = 0
-            while(head is not None):
-                counter +=1
+            while head:
+                counter += 1
                 head = head.next
             return counter
 
-        def split(head, step):
+        def split(head, step) -> ListNode:
+            # split list by steps
             i = 1
-            while (i < step and head):
+            while i < step and head:
                 head = head.next
                 i += 1
+            if not head:
+                return None
+            second_half_head, head.next = head.next, None
+            return second_half_head
 
-            if head is None: return None
-            #disconnect
-            temp, head.next = head.next, None
-            return temp
-
-        def merge(l, r, head):
-            cur = head
-            while(l and r):
+        # merge left and right list by combining them
+        # to head node
+        # return last node of new linked list
+        def merge(l, r, head) -> ListNode:
+            curr = head
+            while l and r:
                 if l.val < r.val:
-                    cur.next, l = l, l.next
+                    curr.next = l
+                    l = l.next
                 else:
-                    cur.next, r = r, r.next
-                cur = cur.next
+                    curr.next = r
+                    r = r.next
+                curr = curr.next
+            curr.next = l or r
+            while curr.next:
+                curr = curr.next
 
-            cur.next = l if l is not None else r
-            while cur.next is not None: cur = cur.next
-            return cur
+            return curr
 
         size = getSize(head)
         bs = 1
-        dummy = ListNode(0)
+        dummy = ListNode(-1)
         dummy.next = head
         l, r, tail = None, None, None
-        while (bs < size):
-            cur = dummy.next
+        while bs < size:
+            curr = dummy.next
             tail = dummy
-            while cur:
-                l = cur
+            while curr:
+                l = curr
                 r = split(l, bs)
-                cur = split(r, bs)
+                curr = split(r, bs)
                 tail = merge(l, r, tail)
             bs <<= 1
         return dummy.next
+
+
+
+
 
