4791a8b1b045cc19d890e7496aa91cedbce72bf1
Makris Huang <vallwesture@gmail.com>
2019-10-30 10:43:19 -0700

    apply a concise sol for prob 394 decode string

--- python/394_decode_string.py
+++ python/394_decode_string.py
@@ -1,30 +1,21 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
-        idx = 0
-        res = ""
-        while idx < len(s):
-            if s[idx].isnumeric():
-                curr_idx = idx + 1
-                while curr_idx < len(s) and s[curr_idx].isnumeric():
-                    curr_idx += 1
-                stack.append(int(s[idx:curr_idx]))
-                idx = curr_idx
-            elif s[idx] == "[":
-                idx += 1
-            elif s[idx].isalpha():
-                curr_idx = idx + 1
-                while curr_idx < len(s) and s[curr_idx].isalpha():
-                    curr_idx += 1
-                stack.append(s[idx:curr_idx])
-                idx = curr_idx
-            elif s[idx] == "]":
-                temp = ""
-                while stack and str(stack[-1]).isalpha():
-                    temp = stack.pop() + temp
-                if stack:
-                    num = stack.pop()
-                    temp = temp * num
-                    stack.append(temp)
-                idx += 1
-        return "".join(stack)
+        curNum = 0
+        curString = ''
+
+        for c in s:
+            if c == '[':
+                stack.append(curString)
+                stack.append(curNum)
+                curString = ''
+                curNum = 0
+            elif c == ']':
+                num = stack.pop()
+                prevString = stack.pop()
+                curString = prevString + num*curString
+            elif c.isdigit():
+                curNum = curNum*10 + int(c)
+            else:
+                curString += c
+        return curString
