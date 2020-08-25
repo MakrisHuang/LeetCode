8173c2d54811bb9a47eaacd19aa4bb1e6bef81c6
Makris Huang <tai-chia.huang@sv.cmu.edu>
2020-08-24 23:38:46 -0700

    create a cycle for not counting nodes

--- python/061_rotate_list.py
+++ python/061_rotate_list.py
@@ -5,6 +5,24 @@
 #         self.next = None
 
 class Solution:
+    # form a cycle and move (count - k) steps, and create a new head
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        if not head or k == 0: return head
+        tail = head
+        count = 1
+        while tail.next:
+            tail = tail.next
+            count += 1
+        tail.next = head
+        k %= count
+        offset = count - k
+        for _ in range(offset):
+            tail = tail.next
+            offset -= 1
+        head = tail.next
+        tail.next = None
+        return head
+
     def rotateRight(self, head: ListNode, k: int) -> ListNode:
         if not head: return head
         leng = 0
