4086afd0256fe0944aa9a3f46d6e7c1be04d3752
Makris Huang <tai-chia.huang@sv.cmu.edu>
2020-09-08 22:47:05 -0700

    make the answer clearer

--- python/543_diameterof_binary_tree.py
+++ python/543_diameterof_binary_tree.py
@@ -1,22 +1,21 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        self.ans = 1
-
-        def dfs(node: TreeNode) -> int:
+        # calculate the length of left node and right node
+        # thinking the depth starting from the bottom of a leaf node
+        self.ans = 0
+        def depth(node):
             if not node: return 0
-
-            l = dfs(node.left)
-            r = dfs(node.right)
-
-            self.ans = max(self.ans, l + r + 1)
-            return max(l, r) + 1
-
-        dfs(root)
-        return self.ans - 1
+            left = depth(node.left)
+            right = depth(node.right)
+            # path
+            self.ans = max(self.ans, left + right)
+            # depth
+            return max(left, right) + 1
+        depth(root)
+        return self.ans
