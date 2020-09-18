eab98d160cccc8e0e41acafbc3c1f63e7be9a3f3
Makris Huang <tai-chia.huang@sv.cmu.edu>
2020-09-17 21:20:11 -0700

    use list and map for prob 380 insert delete getRandom

--- python/380_insert_delete_getrandom_o1.py
+++ python/380_insert_delete_getrandom_o1.py
@@ -1,19 +1,20 @@
+import random
 class RandomizedSet:
-    import random
+
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.map = dict()
-        self.list = list()
+        self.nums = []
+        self.positions = {} # key: number, value: index in self.nums
 
     def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
         """
-        if val not in self.map:
-            self.map[val] = len(self.list)
-            self.list.append(val)
+        if val not in self.positions:
+            self.nums.append(val)
+            self.positions[val] = len(self.nums) - 1
             return True
         else:
             return False
@@ -22,30 +23,22 @@ class RandomizedSet:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
-        if val in self.map:
-            val_idx = self.map[val]
-
-            last_ele = self.list[-1]
-            last_idx = self.map[last_ele]
-
-            self.map[val] = last_idx
-            self.list[last_idx] = val
-
-            self.list[val_idx] = last_ele
-            self.map[last_ele] = val_idx
-
-            self.list[:] = self.list[:-1]
-            del self.map[val]
-
+        if val in self.positions:
+            idx, last = self.positions[val], self.nums[-1]
+            self.nums[idx], self.positions[last] = last, idx
+            self.nums.pop()
+            self.positions.pop(val, 0)
             return True
-        else:
-            return False
+        return False
 
     def getRandom(self) -> int:
         """
         Get a random element from the set.
         """
-        return self.list[random.randrange(len(self.list))]
+        idx = random.randint(0, len(self.nums) - 1)
+        return self.nums[idx]
+
+
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
