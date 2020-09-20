a4959682b6d57b00f9265649fbf9769a1243cfba
Makris Huang <tai-chia.huang@sv.cmu.edu>
2020-09-20 10:54:17 -0700

    add top-down approach

--- python/322_coin_change.py
+++ python/322_coin_change.py
@@ -7,3 +7,22 @@ class Solution:
                 if coin <= i:
                     dp[i] = min(dp[i], dp[i - coin] + 1)
         return dp[amount] if dp[amount] <= amount else -1
+
+    def coinChange_topDown(self, coins: List[int], amount: int) -> int:
+        def recursive(coins: List[int], remaining: int, mem: List[int]) -> int:
+            if remaining < 0:
+                return -1
+            if remaining == 0:
+                return 0
+            if mem[remaining] != 0: return mem[remaining]
+            res = float('inf')
+            for coin in coins:
+                subproblem = recursive(coins, remaining - coin, mem)
+                if subproblem == -1:
+                    continue
+                res = min(res, subproblem + 1)
+            mem[remaining] = res if res != float('inf') else -1
+            return mem[remaining]
+
+        mem = [0 for _ in range(amount + 1)]
+        return recursive(coins, amount, mem)
