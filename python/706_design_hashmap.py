7ac532571d6900c94a1cb385eefeb83ae3a27039
Makris Huang <tai-chia.huang@sv.cmu.edu>
2020-09-22 21:07:58 -0700

    use bucket and chaining for prob 706 design hashmap

--- python/706_design_hashmap.py
+++ python/706_design_hashmap.py
@@ -1,29 +1,67 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+
+
 class MyHashMap:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.bucket = [None] * 1000001
+        self.capacity = 1000
+        self.bucket = [None for _ in range(self.capacity)]
 
     def put(self, key: int, value: int) -> None:
         """
         value will always be non-negative.
         """
-        self.bucket[key] = value
+        index = key % self.capacity
+        if not self.bucket[index]:
+            self.bucket[index] = Node(key, value)
+        else:
+            # collesion, iterate the linked list and find the value
+            curr = self.bucket[index]
+            while curr:
+                if curr.key == key:
+                    curr.value = value
+                    return
+                if not curr.next: break
+                curr = curr.next
+            curr.next = Node(key, value)
 
     def get(self, key: int) -> int:
         """
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
         """
-        val = self.bucket[key]
-        return val if val != None else -1
+        index = key % self.capacity
+        curr = self.bucket[index]
+        while curr:
+            if curr.key == key:
+                return curr.value
+            curr = curr.next
+        return -1
 
     def remove(self, key: int) -> None:
         """
         Removes the mapping of the specified value key if this map contains a mapping for the key
         """
-        self.bucket[key] = None
+        index = key % self.capacity
+        curr = prev = self.bucket[index]
+        if not curr: return
+        if curr.key == key:
+            self.bucket[index] = curr.next
+        else:
+            curr = curr.next
+            while curr:
+                if curr.key == key:
+                    prev.next = curr.next
+                    break
+                else:
+                    curr, prev = curr.next, prev.next
+
 
 
 # Your MyHashMap object will be instantiated and called as such:
