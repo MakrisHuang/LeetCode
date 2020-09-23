f13fdf3ec701166a25a2fcde5effe4ddd895f8c5
Makris Huang <tai-chia.huang@sv.cmu.edu>
2020-09-22 22:04:57 -0700

    add more explaination

--- python/986_interval_list_intersections.py
+++ python/986_interval_list_intersections.py
@@ -2,17 +2,17 @@ class Solution:
     def intervalIntersection(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
         res = []
         i = j = 0
-
         while i < len(A) and j < len(B):
             start = max(A[i][0], B[j][0])
             end = min(A[i][1], B[j][1])
             if start <= end:
                 res.append([start, end])
 
+            # If A[0] has the smallest endpoint, it can only intersect B[0]. After, we can discard A[0] since it cannot intersect anything else.
             if A[i][1] < B[j][1]:
                 i += 1
             else:
+                # Similarly, if B[0] has the smallest endpoint, it can only intersect A[0], and we can discard B[0] after since it cannot intersect anything else.
                 j += 1
-
         return res
 
