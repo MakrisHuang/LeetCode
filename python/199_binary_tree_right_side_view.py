ccf5aab66a1002ffd4e5e6f79400352372095e11
Makris Huang <tai-chia.huang@sv.cmu.edu>
2020-09-23 19:18:37 -0700

    use level traversal

--- python/199_binary_tree_right_side_view.py
+++ python/199_binary_tree_right_side_view.py
@@ -1,27 +1,43 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def rightSideView(self, root: TreeNode) -> List[int]:
-        # apply bfs to print out the last node in each level
+        # use BFS and load the last element
+        from collections import deque
+        q = deque()
         res = []
-        q = collections.deque()
-        if root:
-            q.append(root)
+        if not root: return []
+        q.append(root)
         while q:
-            size = len(q)
-            val = None
-            for i in range(size):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-                val = node.val
-            res.append(val)
+            N = len(q)
+            for i in range(N):
+                curr = q.popleft()
+                if curr.left:
+                    q.append(curr.left)
+                if curr.right:
+                    q.append(curr.right)
+                if i == N - 1:
+                    res.append(curr.val)
         return res
 
+    def rightSideView_levelTraversal(self, root: TreeNode) -> List[int]:
+        def levelVisit(res: List[List[int]], level: int, node: TreeNode):
+            if not node: return
+            if len(res) < level:
+                res.append([])
+            levelVisit(res, level + 1, node.left)
+            row = res[level - 1]
+            row.append(node.val)
+            res[level - 1] = row
+            levelVisit(res, level + 1, node.right)
+
+        levels = []
+        levelVisit(levels, 1, root)
+        res = []
+        for row in levels:
+            res.append(row[-1])
+        return res
