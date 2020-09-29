0f5ae2804db1c52dd749aca8c85add97aeae4d7d
Makris Huang <tai-chia.huang@sv.cmu.edu>
2020-09-28 18:05:58 -0700

    add sol for prob 158 read N characters given read4 II call multiple times

---
+++ python/158_read_N_characters_given_read4_II_call_multiple_times.py
@@ -0,0 +1,20 @@
+# The read4 API is already defined for you.
+# def read4(buf4: List[str]) -> int:
+
+class Solution:
+    def __init__(self):
+        self.q = []
+
+    def read(self, buf: List[str], n: int) -> int:
+        i = 0
+        while i < n:
+            if self.q:
+                buf[i] = self.q.pop(0)  # always enter this block to update buf
+                i += 1
+            else:                       # we only count characters read here, and push them into queue
+                buf4 = [''] * 4
+                v = read4(buf4)
+                if v == 0:
+                    break
+                self.q += buf4[:v]
+        return i
