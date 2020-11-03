5799fa18a335245edd1b71416193227112761ccc
Makris Huang <vallwesture@gmail.com>
2020-11-02 16:46:59 -0800

    simplify code

--- python/554_brick_wall.py
+++ python/554_brick_wall.py
@@ -1,21 +1,14 @@
 class Solution:
-    # Time Complexity: O(W * H) (W = average bricks in a row, H = num of rows)
     def leastBricks(self, wall: List[List[int]]) -> int:
-        for row in range(len(wall)):
-            for col in range(1, len(wall[row])):
-                wall[row][col] += wall[row][col - 1]
-        gap_map = dict()
-        for row in range(len(wall)):
-            for col in range(len(wall[row]) - 1):
-                count = gap_map.setdefault(wall[row][col], 0)
-                count += 1
-                gap_map[wall[row][col]] = count
-
-        # find out top frequent number (has the most gap)
-        largest_gaps = 0
-        for width, count in gap_map.items():
-            if count > largest_gaps:
-                largest_gaps = count
-        height = len(wall)
-        return height - largest_gaps
-
+        for i in range(len(wall)):
+            wall[i].pop()
+            for j in range(1, len(wall[i])):
+                wall[i][j] += wall[i][j - 1]
+        count_map = collections.defaultdict(int)
+        for w in wall:
+            for num in w:
+                count_map[num] += 1
+        key_with_max_value = 0
+        if count_map:
+            key_with_max_value = max(count_map, key=count_map.get)
+        return len(wall) - count_map[key_with_max_value]
