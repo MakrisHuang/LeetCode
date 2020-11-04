c536457fe2a0e6617d2723bd8b28d77a48ce98c3
Makris Huang <vallwesture@gmail.com>
2020-11-03 20:32:36 -0800

    add answer of using two pointer

--- python/350_intersection_of_two_arrays_II.py
+++ python/350_intersection_of_two_arrays_II.py
@@ -1,5 +1,5 @@
 class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    def intersect_hash(self, nums1: List[int], nums2: List[int]) -> List[int]:
         m1 = dict(collections.Counter(nums1))
         m2 = dict(collections.Counter(nums2))
         keys = set(m1.keys()) & set(m2.keys())
@@ -8,3 +8,21 @@ class Solution:
             min_count = min(m1[key], m2[key])
             res += [key] * min_count
         return res
+
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        i, j, k = 0, 0, 0
+        res = []
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                nums1[k] = nums1[i]
+                k += 1
+                i += 1
+                j += 1
+        return nums1[:k]
+
