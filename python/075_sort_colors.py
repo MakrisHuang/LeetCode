3039dc58ac75f3cbdf172e2bb4ea43b8bf58d4b7
Makris Huang <vallwesture@gmail.com>
2020-11-06 21:42:00 -0800

    use two pointer

--- python/075_sort_colors.py
+++ python/075_sort_colors.py
@@ -1,14 +1,41 @@
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+    def sortColors_detailed(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        map = dict(collections.Counter(nums))
-        index = 0
-        for color in [0, 1, 2]:
-            if color in map:
-                count = map[color]
-                while count > 0:
-                    nums[index] = color
-                    count -= 1
-                    index += 1
+        left, right = 0, len(nums) - 1
+        i = 0
+        while i <= right:
+            if nums[i] == 0:
+                # swap nums[i] with left pointer
+                nums[left], nums[i] = nums[i], nums[left]
+                left += 1
+                i += 1
+            elif nums[i] == 2:
+                # swap nums[i] with right pointer
+                nums[right], nums[i] = nums[i], nums[right]
+                right -= 1
+            else:
+                i += 1
+
+    """
+        r = where next red to store (A[r]=0 when r==i or A[r]=1 after first 1 occurs)
+        b = where next blue to store (unknown value)
+              r   i     b
+        0 0 0 1 1 ? ... ? 2 2 2
+    """
+    def sortColors(self, nums: List[int]) -> None:
+
+        left, right = 0, len(nums) - 1
+        i = 0
+        while i <= right:
+            if nums[i] == 0:
+                nums[left], nums[i] = nums[i], nums[left]
+                left += 1
+            elif nums[i] == 2: # Continue handle i after swap, since we don't know A[b]
+                nums[right], nums[i] = nums[i], nums[right]
+                right -= 1
+                i -= 1
+            i += 1
+
+
