c096f4d9c679c63ebdee4b4b8c8953e358709951
Makris Huang <vallwesture@gmail.com>
2021-11-15 00:15:49 -0800

    Make code easier to read

--- python/133_clone_graph.py
+++ python/133_clone_graph.py
@@ -1,18 +1,27 @@
 """
 # Definition for a Node.
 class Node:
-    def __init__(self, val, neighbors):
+    def __init__(self, val = 0, neighbors = None):
         self.val = val
-        self.neighbors = neighbors
+        self.neighbors = neighbors if neighbors is not None else []
 """
+
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        visited = dict()
+        visited = {}
 
-        def dfs(old):
-            if old not in visited:
-                visited[old] = new = Node(old.val, [])
+        def dfs(node: 'Node'):
+            if not node:
+                return None
+            if (node in visited):
+                return visited[node]
+            new_node = Node(node.val)
+            visited[node] = new_node
+            for nei in node.neighbors:
+                new_node.neighbors.append(dfs(nei))
+            return new_node
 
-                new.neighbors = [*map(dfs, old.neighbors)]
-            return visited[old]
-        return dfs(node)
+        res = dfs(node)
+        return res
+
+
