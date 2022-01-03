e297f948c4573dc14c15784ab5e26125e4f63a1c
Makris Huang <vallwesture@gmail.com>
2022-01-03 10:48:33 +0800

    Add description for prob 121 best time to buy and sell stock

--- python/121_best_time_to_buy_and_sell_stock.py
+++ python/121_best_time_to_buy_and_sell_stock.py
@@ -1,10 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = float('inf')
-        max_profit = 0
-        for price in prices:
-            if price < min_price:
-                min_price = price
-            if price - min_price > max_profit:
-                max_profit = price - min_price
-        return max_profit
+        lowest = prices[0]
+        profit = 0
+        for i in range(1, len(prices)):
+            profit = max(profit, prices[i] - lowest)
+            if prices[i] < lowest:  # update the lowest since we only can sell after buy
+                lowest = prices[i]
+        return profit
