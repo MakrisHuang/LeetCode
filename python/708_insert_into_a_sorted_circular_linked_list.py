febc8a36a28c0ab7896763cec0a5462f5f65d417
Makris Huang <vallwesture@gmail.com>
2022-01-19 13:20:50 +0800

    update sol for prob 708 insert into a sorted circular linked list

--- python/708_insert_into_a_sorted_circular_linked_list.py
+++ python/708_insert_into_a_sorted_circular_linked_list.py
@@ -7,37 +7,36 @@ class Node:
 """
 
 class Solution:
-    def insert(self, head: 'Node', insertVal: int) -> 'Node':
+    def insert(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
+        # case 1: prev < insertVal < head
+        # case 2: insertVal > curr > prev => moving curr and prev to next
+        # ex. 5 => moving until curr is smaller than prev
+        # case 3: curr > prev > insertVal => ex. -5 => moving until curr is smaller than prev
+
+        new_node = Node(insertVal)
         if not head:
-            new_node = Node(insertVal, head)
             new_node.next = new_node
             return new_node
 
-        prev = head
-        cur = head.next
+        prev, curr = head, head.next
+        toInsert = False
+
         while True:
-            # Found a spot between smaller and bigger node.
-            if prev.val <= insertVal <= cur.val:
-                break
-            # Reached inflection point. This is for catching `insertVal`s
-            # that are smaller than min or bigger than max.
-            elif (prev.val > cur.val and
-                  # We might have hit the inflection point because the given
-                  # head started us off past the expected insertion point. So
-                  # this is only the insertion point if it fits the order.
-                  # `or` because it cannot be both bigger than max and smaller than min.
-                    (insertVal <= cur.val or insertVal >= prev.val)):
-                break
-            # Put this *before* next `if` condition to skip the very first
-            # loop iteration (when `prev` is always `head`).
-            prev, cur = prev.next, cur.next
-            # Means we made a complete cycle without breaking, meaning
-            # all nodes are dupe vals (prev<=val<=cur doesn't hit because
-            # val could be larger or smaller than the dupe).
+            if prev.val <= insertVal <= curr.val:
+                toInsert = True
+
+            elif prev.val > curr.val:
+                if insertVal >= prev.val or insertVal <= curr.val:
+                    toInsert = True
+
+            if toInsert:
+                prev.next = Node(insertVal, curr)
+                return head
+
+            prev, curr = curr, curr.next
             if prev == head:
                 break
 
-        node = Node(insertVal, cur)
-        prev.next = node
+        # handle case 3 where all values are identical
+        prev.next = Node(insertVal, curr)
         return head
-
