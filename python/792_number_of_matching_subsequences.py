5c7ab4b101038ef41bed1cccde96a3c81c786a96
Makris Huang <vallwesture@gmail.com>
2022-01-31 03:31:45 +0800

    Use iterator for prob 792 number of matching subsequences

--- python/792_number_of_matching_subsequences.py
+++ python/792_number_of_matching_subsequences.py
@@ -1,26 +1,21 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        def ifMatch(s, w):
-            s_len = len(s)
-            w_len = len(w)
-            if w_len > s_len: return False
-            s_idx = w_idx = 0
-            while w_idx < w_len:
-                while s_idx < s_len and s[s_idx] != w[w_idx]:
-                    s_idx += 1
+        head = [[] for _ in range(26)]
+        for word in words:
+            it = iter(word)
+            head[ord(next(it)) - ord('a')].append(it)
 
-                if s_idx == s_len:
-                    break
-                if s[s_idx] == w[w_idx]:
-                    s_idx += 1
-                    w_idx += 1
-            if w_idx < w_len: return False
-            else: return True
+        ans = 0
+        for letter in s:
+            index = ord(letter) - ord('a')
+            old_bucket = head[index]
+            head[index] = []
 
-        count = 0
-        words2Count = collections.Counter(words)
-        for word, occur in words2Count.items():
-            if ifMatch(s, word):
-                count += occur
-        return count
-
+            while old_bucket:
+                curr_iterator = old_bucket.pop()
+                next_letter = next(curr_iterator, None)
+                if next_letter:
+                    head[ord(next_letter) - ord('a')].append(curr_iterator)
+                else:
+                    ans += 1
+        return ans
