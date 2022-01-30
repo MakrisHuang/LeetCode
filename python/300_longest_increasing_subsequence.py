9b2d91be5696a8d7bfdd15eeaf3cfb4954f4db3e
Makris Huang <vallwesture@gmail.com>
2022-01-31 03:51:28 +0800

    Use binary search to reduce time complexity

--- python/300_longest_increasing_subsequence.py
+++ python/300_longest_increasing_subsequence.py
@@ -1,21 +1,42 @@
 class Solution:
-    # Time Complexity: O(n * 2)
-    # Space Complexity: O(n)
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # apply dynamic programming
-        # f(i) = max(curr_idx, dp[j]), j < i
-        if not nums: return 0
+        # in brute force it gives us O(n^2) time complexity
+        # here we will maintain a global array that store the longest chain
+        # To maintain this, every time we iterate the element, we insert the
+        # elements with binary search. This will give us O(NlogN) time complexity
+        def binary_search_get_index(nums, target) -> int:
+            left, right = 0, len(nums) - 1
+            while left < right:
+                mid = left + (right - left) // 2
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid
+            return left
 
-        dp = [0] * len(nums)
-        dp[0] = 1
-        res = 1
-        for i in range(1, len(nums)):
-            curr_max = 0
-            # record how many numbers that are smaller than me
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    curr_max = max(curr_max, dp[j])
-            dp[i] = curr_max + 1
-            res = max(res, dp[i])
-        return res
+        sub = [nums[0]]
+        for num in nums[1:]:
+            if num > sub[-1]:
+                sub.append(num)
+            else:
+                i = binary_search_get_index(sub, num)
+                sub[i] = num
+        return len(sub)
+
+    def lengthOfLIS_python_binary_search(self, nums: List[int]) -> int:
+        sub = []
+        for num in nums:
+            i = bisect_left(sub, num)
+
+            # If num is greater than any element in sub
+            if i == len(sub):
+                sub.append(num)
+
+            # Otherwise, replace the first element in sub greater than or equal to num
+            else:
+                sub[i] = num
+
+        return len(sub)
 
