521a97b83e6a204cfa6a73eb2c81f3b496aa9efb
Makris Huang <vallwesture@gmail.com>
2022-07-04 11:41:13 -0700

    Add N*logK answer

--- python/973_k_closest_points_to_origin.py
+++ python/973_k_closest_points_to_origin.py
@@ -1,4 +1,18 @@
 class Solution:
+    # Complexity: O(N * logK) (N = number of nodes, K = K cloest)
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap = []
+
+        for (x, y) in points:
+            dist = -(x * x + y * y)
+            if len(heap) == k:
+                heapq.heappushpop(heap, (dist, x, y))
+            else:
+                heapq.heappush(heap, (dist, x, y))
+
+        return [[x, y] for (heap, x, y) in heap ]
+
+    # O(K + N*logN)
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
         q = []
         for x, y in points:
