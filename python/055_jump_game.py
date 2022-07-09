5ff5159f2b06ef09dfc91690721ba76d96d1379c
Makris Huang <vallwesture@gmail.com>
2022-07-09 15:01:05 -0700

    Update answer for prob 055 jump game

--- python/055_jump_game.py
+++ python/055_jump_game.py
@@ -1,11 +1,14 @@
 class Solution:
-    """
-    Iterating right-to-left, for each position we check if there is a potential jump that reaches a GOOD index (currPosition + nums[currPosition] >= leftmostGoodIndex). If we can reach a GOOD index, then our position is itself GOOD. Also, this new GOOD position will be the new leftmost GOOD index. Iteration continues until the beginning of the array. If first position is a GOOD index then we can reach the last index from the first position.
-    """
     def canJump(self, nums: List[int]) -> bool:
-        N = len(nums)
-        last_idx = N - 1
-        for i in range(N - 1, -1, -1):
-            if i + nums[i] >= last_idx:
-                last_idx = i
-        return last_idx == 0
+        # this can be made in dynamic programming but we can also thinking from end index
+        # if we start from endpoition, can we reach the index at 0?
+        last_position = len(nums) - 1
+
+        # starting at second last item since we want to know if we can reach to last one
+        # by adding nums[i] to current index
+        for i in range(len(nums) - 2, -1, -1):
+            # ff this index has jump count which can reach to or beyond the last position
+            if i + nums[i] >= last_position:
+                # since we just need to reach this destination
+                last_position = i
+        return last_position == 0
